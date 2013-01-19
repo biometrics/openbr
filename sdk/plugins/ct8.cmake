e035e21e621c6981f46e1fabc0890f1561327d85
caotto <ottochar@gmail.com>
2013-01-19 16:44:29 -0500

    Update ct8 cmake files
    Target version 8.6.0 of the sdk (this could stand to be generalized somehow)
    Link debug versions of the library when building debug configurations.
    When building with visual studio, pick the appropriate pre-complied library
    for the version of the compiler being used.

--- sdk/plugins/ct8.cmake
+++ sdk/plugins/ct8.cmake
@@ -3,7 +3,10 @@ set(BR_WITH_CT8 OFF CACHE BOOL "Build with Cognitec FaceVACS 8")
 if(${BR_WITH_CT8})
   find_package(CT8 REQUIRED)
   set(BR_THIRDPARTY_SRC ${BR_THIRDPARTY_SRC} plugins/ct8.cpp ${CT8_RESOURCES})
+
+  set(CT8_LIBS optimized ${CT8_LIBRARY_RELEASE} debug ${CT8_LIBRARY_DEBUG})
   set(BR_THIRDPARTY_LIBS ${BR_THIRDPARTY_LIBS} ${CT8_LIBS})
+
   install(FILES ${CT8_DIR_LIB}/libfrsdk-8.5.0.so
                 ${CT8_DIR_LIB}/../share/libhasp_linux_x86_64_67109.so
                 ${CT8_DIR_LIB}/../share/libiomp5.so
