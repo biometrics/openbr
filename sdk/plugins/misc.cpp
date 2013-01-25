f74da8ddc6714635b1d5604587abbff3d7a2f413
Josh Klontz <josh.klontz@gmail.com>
2013-01-25 11:09:09 -0500

    added CheckTransform

--- sdk/plugins/misc.cpp
+++ sdk/plugins/misc.cpp
@@ -117,6 +117,32 @@ BR_REGISTER(Transform, PrintTransform)
 
 /*!
  * \ingroup transforms
+ * \brief Checks the template for NaN values.
+ * \author Josh Klontz \cite jklontz
+ */
+class CheckTransform : public UntrainableMetaTransform
+{
+    Q_OBJECT
+
+    void project(const Template &src, Template &dst) const
+    {
+        dst = src;
+        foreach (const Mat &m, src) {
+            Mat fm;
+            m.convertTo(fm, CV_32F);
+            const int elements = fm.rows * fm.cols * fm.channels();
+            const float *data = (const float*)fm.data;
+            for (int i=0; i<elements; i++)
+                if (data[i] != data[i])
+                    qFatal("%s NaN check failed!", qPrintable(src.file.flat()));
+        }
+    }
+};
+
+BR_REGISTER(Transform, CheckTransform)
+
+/*!
+ * \ingroup transforms
  * \brief Sets the template's matrix data to the br::File::name.
  * \author Josh Klontz \cite jklontz
  */
