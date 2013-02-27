c1535bee409fc5fcf324ca9f1e0d5b09150fa4bb
Josh Klontz <josh.klontz@gmail.com>
2013-02-27 10:28:06 -0500

    added reduce transform

--- sdk/plugins/reduce.cpp
+++ sdk/plugins/reduce.cpp
@@ -79,6 +79,46 @@ class AndTransform : public UntrainableMetaTransform
 
 BR_REGISTER(Transform, AndTransform)
 
+/*!
+ * \ingroup transforms
+ * \brief Statistics
+ * \author Josh Klontz \cite jklontz
+ */
+class StatTransform : public UntrainableTransform
+{
+    Q_OBJECT
+    Q_ENUMS(Statistic)
+    Q_PROPERTY(Statistic statistic READ get_statistic WRITE set_statistic RESET reset_statistic STORED false)
+
+public:
+    /*!
+     * \brief Available statistics
+     */
+    enum Statistic { Min, Max, Mean, StdDev };
+
+private:
+    BR_PROPERTY(Statistic, statistic, Mean)
+
+    void project(const Template &src, Template &dst) const
+    {
+        if (src.m().channels() != 1)
+            qFatal("Expected 1 channel matrix.");
+        Mat m(1, 1, CV_32FC1);
+        if ((statistic == Min) || (statistic == Max)) {
+            double min, max;
+            minMaxLoc(src, &min, &max);
+            m.at<float>(1, 1) = (statistic == Min ? min : max);
+        } else {
+            Scalar mean, stddev;
+            meanStdDev(src, mean, stddev);
+            m.at<float>(1,1) = (statistic == Mean ? mean[0] : stddev[0]);
+        }
+        dst = m;
+    }
+};
+
+BR_REGISTER(Transform, StatTransform)
+
 } // namespace br
 
 #include "reduce.moc"
