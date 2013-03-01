b89cf44866a59fcccdaafa34d24286e2291711a1
Scott Klum <scott.klum@gmail.com>
2013-03-01 14:35:58 -0500

    Fixed rankOutput to work with non-self identity masks

--- sdk/plugins/output.cpp
+++ sdk/plugins/output.cpp
@@ -271,7 +271,8 @@ class rankOutput : public MatrixOutput
         if (targetFiles.isEmpty() || queryFiles.isEmpty()) return;
 
         QList<int> ranks;
-        QList<double> scores;
+        QList<int> positions;
+        QList<float> scores;
         QStringList lines;
 
         for (int i=0; i<queryFiles.size(); i++) {
@@ -280,6 +281,7 @@ class rankOutput : public MatrixOutput
             foreach (const Pair &pair, Common::Sort(OpenCVUtils::matrixToVector(data.row(i)), true)) {
                 if(targetFiles[pair.second].label() == queryFiles[i].label()) {
                     ranks.append(rank);
+                    positions.append(pair.second);
                     scores.append(pair.first);
                     break;
                 }
@@ -289,7 +291,7 @@ class rankOutput : public MatrixOutput
 
         typedef QPair<int,int> RankPair;
         foreach (const RankPair &pair, Common::Sort(ranks, false))
-            lines.append(queryFiles[pair.second].name + " " + QString::number(pair.first) + " " + QString::number(scores[pair.second]) + " " + targetFiles[pair.second].name);
+            lines.append(queryFiles[pair.second].name + " " + QString::number(pair.first) + " " + QString::number(scores[pair.second]) + " " + targetFiles[positions[pair.second]].name);
 
         QtUtils::writeFile(file, lines);
     }
