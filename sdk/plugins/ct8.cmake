64a36c1c459bf2e4bf6820a0bc4c10a652c24a10
Scott Klum <scott.klum@gmail.com>
2013-03-06 14:21:10 -0500

    CT8 fix

--- sdk/plugins/ct8.cmake
+++ sdk/plugins/ct8.cmake
@@ -7,10 +7,16 @@ if(${BR_WITH_CT8})
   set(CT8_LIBS optimized ${CT8_LIBRARY_RELEASE} debug ${CT8_LIBRARY_DEBUG})
   set(BR_THIRDPARTY_LIBS ${BR_THIRDPARTY_LIBS} ${CT8_LIBS})
 
+
+  if(WIN32)
+  install(FILES ${CT8_DIR_LIB}/libfrsdk-8.6.0.dll
+          DESTINATION bin)
+  install(DIRECTORY ${CT8_DIR}/etc DESTINATION models/ct8)
+  add_definitions(-DCT8_DIR=\"${CT8_DIR}\")
+  else()
   install(FILES ${CT8_DIR_LIB}/libfrsdk-8.5.0.so
                 ${CT8_DIR_LIB}/../share/libhasp_linux_x86_64_67109.so
                 ${CT8_DIR_LIB}/../share/libiomp5.so
           DESTINATION lib)
-  install(DIRECTORY ${CT8_DIR}/etc DESTINATION models/ct8)
-  add_definitions(-DCT8_DIR=\"${CT8_DIR}\")
+  endif()
 endif()
