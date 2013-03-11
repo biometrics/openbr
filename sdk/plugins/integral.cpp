3021f01b466282570872830eaeedbc392c119b0c
Josh Klontz <josh.klontz@gmail.com>
2013-03-11 17:02:10 -0400

    started writing a new algorithm

--- sdk/plugins/integral.cpp
+++ sdk/plugins/integral.cpp
@@ -1,5 +1,8 @@
 #include <opencv2/imgproc/imgproc.hpp>
+#include <Eigen/Core>
 #include <openbr_plugin.h>
+
+using namespace cv;
 
 namespace br
 {
@@ -15,11 +18,76 @@ class IntegralTransform : public UntrainableTransform
 
     void project(const Template &src, Template &dst) const
     {
-        cv::integral(src, dst);
+        integral(src, dst);
     }
 };
 
 BR_REGISTER(Transform, IntegralTransform)
+
+/*!
+ * \ingroup transforms
+ * \brief Sliding window object recognition from a multi-channel intergral image.
+ * \author Josh Klontz \cite jklontz
+ */
+class IntegralSampler : public UntrainableTransform
+{
+    Q_OBJECT
+    Q_PROPERTY(int scales READ get_scales WRITE set_scales RESET reset_scales STORED false)
+    Q_PROPERTY(float scaleFactor READ get_scaleFactor WRITE set_scaleFactor RESET reset_scaleFactor STORED false)
+    Q_PROPERTY(float stepFactor READ get_stepFactor WRITE set_stepFactor RESET reset_stepFactor STORED false)
+    Q_PROPERTY(int minSize READ get_minSize WRITE set_minSize RESET reset_minSize STORED false)
+    BR_PROPERTY(int, scales, 1)
+    BR_PROPERTY(float, scaleFactor, 1.25)
+    BR_PROPERTY(float, stepFactor, 0.25)
+    BR_PROPERTY(int, minSize, 8)
+
+    void project(const Template &src, Template &dst) const
+    {
+        typedef Eigen::Map< const Eigen::Matrix<qint32,Eigen::Dynamic,1> > InputDescriptor;
+        typedef Eigen::Map< Eigen::Matrix<qint32,Eigen::Dynamic,1> > OutputDescriptor;
+        const Mat &m = src.m();
+        if (m.depth() != CV_32S) qFatal("Expected CV_32S matrix depth.");
+        const int channels = m.channels();
+        const int rowStep = channels * m.cols;
+
+        int descriptors = 0;
+        int currentSize = min(m.rows, m.cols)-1;
+        for (int scale=0; scale<scales; scale++) {
+            descriptors += ceil((m.rows-currentSize)*stepFactor/currentSize) *
+                    ceil((m.cols-currentSize)*stepFactor/currentSize);
+            currentSize /= scaleFactor;
+            if (currentSize < minSize)
+                break;
+        }
+        Mat n(descriptors, channels, CV_32SC1);
+
+        const qint32 *dataIn = (qint32*)m.data;
+        qint32 *dataOut = (qint32*)n.data;
+        currentSize = min(m.rows, m.cols)-1;
+        int index = 0;
+        for (int scale=0; scale<scales; scale++) {
+            const int currentStep = currentSize * stepFactor;
+            for (int i=currentSize; i<m.rows; i+=currentStep) {
+                for (int j=currentSize; j<m.cols; j+=currentStep) {
+                    InputDescriptor a(dataIn+((i-currentSize)*rowStep+(j-currentSize)*channels), channels, 1);
+                    InputDescriptor b(dataIn+((i-currentSize)*rowStep+ j             *channels), channels, 1);
+                    InputDescriptor c(dataIn+( i             *rowStep+(j-currentSize)*channels), channels, 1);
+                    InputDescriptor d(dataIn+( i             *rowStep+ j             *channels), channels, 1);
+                    OutputDescriptor y(dataOut+(index*channels), channels, 1);
+                    y = d-b-c+a;
+                    index++;
+                }
+            }
+            currentSize /= scaleFactor;
+            if (currentSize < minSize)
+                break;
+        }
+
+        dst.m() = n;
+    }
+};
+
+BR_REGISTER(Transform, IntegralSampler)
 
 /*!
  * \ingroup transforms
@@ -41,10 +109,10 @@ private:
     void project(const Template &src, Template &dst) const
     {
         if (src.m().type() != CV_8UC1) qFatal("Requires CV_8UC1 input.");
-        cv::Mat dx, dy, magnitude, angle;
-        cv::Sobel(src, dx, CV_32F, 1, 0);
-        cv::Sobel(src, dy, CV_32F, 0, 1);
-        cv::cartToPolar(dx, dy, magnitude, angle, true);
+        Mat dx, dy, magnitude, angle;
+        Sobel(src, dx, CV_32F, 1, 0);
+        Sobel(src, dy, CV_32F, 0, 1);
+        cartToPolar(dx, dy, magnitude, angle, true);
         if ((channel == Magnitude) || (channel == MagnitudeAndAngle))
             dst.append(magnitude);
         if ((channel == Angle) || (channel == MagnitudeAndAngle))
