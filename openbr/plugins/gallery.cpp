3f860cc99bf609824a70362eb974e20b6b3a3d51
Josh Klontz <josh.klontz@gmail.com>
2013-04-10 22:34:17 -0400

    added matrixGallery

--- openbr/plugins/gallery.cpp
+++ openbr/plugins/gallery.cpp
@@ -212,7 +212,49 @@ class DefaultGallery : public Gallery
         format->write(t);
     }
 };
+
 BR_REGISTER(Gallery, DefaultGallery)
+
+/*!
+ * \ingroup galleries
+ * \brief Combine all templates into one large matrix and process it as a br::Format
+ * \author Josh Klontz \cite jklontz
+ */
+class matrixGallery : public Gallery
+{
+    Q_OBJECT
+    Q_PROPERTY(const QString extension READ get_extension WRITE set_extension RESET reset_extension STORED false)
+    BR_PROPERTY(QString, extension, "mtx")
+
+    TemplateList templates;
+
+    ~matrixGallery()
+    {
+        if (templates.isEmpty())
+            return;
+
+        QScopedPointer<Format> format(Factory<Format>::make(getFormat()));
+        format->write(Template(file, OpenCVUtils::toMat(templates.data())));
+    }
+
+    File getFormat() const
+    {
+        return file.name.left(file.name.size() - file.suffix().size()) + extension;
+    }
+
+    TemplateList readBlock(bool *done)
+    {
+        *done = true;
+        return TemplateList() << getFormat();
+    }
+
+    void write(const Template &t)
+    {
+        templates.append(t);
+    }
+};
+
+BR_REGISTER(Gallery, matrixGallery)
 
 /*!
  * \ingroup galleries
