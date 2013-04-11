b452c4c7588164eaf5f36c5d6660ebff85b561e0
Josh Klontz <josh.klontz@gmail.com>
2013-04-10 21:32:18 -0400

    added Reshape transform

--- openbr/plugins/regions.cpp
+++ openbr/plugins/regions.cpp
@@ -111,6 +111,25 @@ BR_REGISTER(Transform, CatTransform)
 
 /*!
  * \ingroup transforms
+ * \brief Reshape the each matrix to the specified number of rows.
+ * \author Josh Klontz \cite jklontz
+ */
+class ReshapeTransform : public UntrainableTransform
+{
+    Q_OBJECT
+    Q_PROPERTY(int rows READ get_rows WRITE set_rows RESET reset_rows STORED false)
+    BR_PROPERTY(int, rows, 1)
+
+    void project(const Template &src, Template &dst) const
+    {
+        dst = src.m().reshape(src.m().channels(), rows);
+    }
+};
+
+BR_REGISTER(Transform, ReshapeTransform)
+
+/*!
+ * \ingroup transforms
  * \brief Wraps OpenCV merge
  * \author Josh Klontz \cite jklontz
  */
