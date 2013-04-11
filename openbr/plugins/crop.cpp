569481dd5730b5953112fd9c74d464c0fda96e7f
Josh Klontz <josh.klontz@gmail.com>
2013-04-11 15:05:01 -0400

    added CropSquareTransform

--- openbr/plugins/crop.cpp
+++ openbr/plugins/crop.cpp
@@ -174,6 +174,25 @@ class SubdivideTransform : public UntrainableTransform
 
 BR_REGISTER(Transform, SubdivideTransform)
 
+/*!
+ * \ingroup transforms
+ * \brief Trim the image so the width and the height are the same size.
+ * \author Josh Klontz \cite jklontz
+ */
+class CropSquareTransform : public UntrainableTransform
+{
+    Q_OBJECT
+
+    void project(const Template &src, Template &dst) const
+    {
+        const Mat &m = src;
+        const int newSize = min(m.rows, m.cols);
+        dst = Mat(m, Rect((m.cols-newSize)/2, (m.rows-newSize)/2, newSize, newSize));
+    }
+};
+
+BR_REGISTER(Transform, CropSquareTransform)
+
 } // namespace br
 
 #include "crop.moc"
