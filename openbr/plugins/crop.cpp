331ac40ab53ccb2767a4749f2b34268e54fe86db
Josh Klontz <josh.klontz@gmail.com>
2013-04-10 22:06:02 -0400

    added Subdivide transform, tweaked GridTransform

--- openbr/plugins/crop.cpp
+++ openbr/plugins/crop.cpp
@@ -151,6 +151,29 @@ class CropBlackTransform : public UntrainableTransform
 
 BR_REGISTER(Transform, CropBlackTransform)
 
+/*!
+ * \ingroup transforms
+ * \brief Divide the matrix into 4 smaller matricies of equal size.
+ * \author Josh Klontz \cite jklontz
+ */
+class SubdivideTransform : public UntrainableTransform
+{
+    Q_OBJECT
+
+    void project(const Template &src, Template &dst) const
+    {
+        const Mat &m = src;
+        const int subrows = m.rows/2;
+        const int subcolumns = m.cols/2;
+        dst.append(Mat(m,Rect(0,          0, subcolumns, subrows)).clone());
+        dst.append(Mat(m,Rect(subcolumns, 0, subcolumns, subrows)).clone());
+        dst.append(Mat(m,Rect(0,          subrows, subcolumns, subrows)).clone());
+        dst.append(Mat(m,Rect(subcolumns, subrows, subcolumns, subrows)).clone());
+    }
+};
+
+BR_REGISTER(Transform, SubdivideTransform)
+
 } // namespace br
 
 #include "crop.moc"
