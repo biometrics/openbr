99e31d428c3b1c50134cbf598cab93f8db434ae0
Josh Klontz <josh.klontz@gmail.com>
2013-04-25 12:07:59 -0400

    added NegativeLogPlusOneDistance

--- openbr/plugins/distance.cpp
+++ openbr/plugins/distance.cpp
@@ -241,6 +241,40 @@ BR_REGISTER(Distance, HalfByteL1Distance)
 
 /*!
  * \ingroup distances
+ * \brief Returns -log(distance(a,b)+1)
+ * \author Josh Klontz \cite jklontz
+ */
+class NegativeLogPlusOneDistance : public Distance
+{
+    Q_OBJECT
+    Q_PROPERTY(br::Distance* distance READ get_distance WRITE set_distance RESET reset_distance STORED false)
+    BR_PROPERTY(br::Distance*, distance, NULL)
+
+    void train(const TemplateList &src)
+    {
+        distance->train(src);
+    }
+
+    float compare(const Template &a, const Template &b) const
+    {
+        return -log(distance->compare(a,b)+1);
+    }
+
+    void store(QDataStream &stream) const
+    {
+        distance->store(stream);
+    }
+
+    void load(QDataStream &stream)
+    {
+        distance->load(stream);
+    }
+};
+
+BR_REGISTER(Distance, NegativeLogPlusOneDistance)
+
+/*!
+ * \ingroup distances
  * \brief Returns \c true if the templates are identical, \c false otherwise.
  * \author Josh Klontz \cite jklontz
  */
