e6a7e66d6296bebd3e9fe18037be5f6638033889
Scott Klum <scott.klum@gmail.com>
2013-06-25 13:53:28 -0400

    Added new stasm cmake files

---
+++ share/openbr/cmake/FindStasm3.cmake
@@ -0,0 +1,20 @@
+# ================================================================
+# The Stasm CMake configuration file
+#
+# Usage from an external project:
+#   In your CMakeLists.txt, add these lines:
+#
+#   find_package(Stasm3 REQUIRED)
+#   target_link_libraries(MY_TARGET ${Stasm_LIBS})
+# ================================================================
+
+find_path(Stasm_DIR include/stasm.hpp ${CMAKE_SOURCE_DIR}/3rdparty/*)
+
+add_subdirectory(${Stasm_DIR} ${Stasm_DIR}/build)
+
+set(SRC ${SOURCE};${SRC})
+
+include_directories(${Stasm_DIR}/include)
+link_directories(${Stasm_DIR}/build)
+
+set(Stasm_LIBS stasm)
