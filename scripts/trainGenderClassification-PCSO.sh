b5caf802f2b6f7e0e621696f39c285c202cf398b
Charles Otto <ottochar@gmail.com>
2013-08-02 12:20:24 -0400

    Update training/eval scripts

--- scripts/trainGenderClassification-PCSO.sh
+++ scripts/trainGenderClassification-PCSO.sh
@@ -6,6 +6,11 @@ fi
 
 #rm -f ../share/openbr/models/features/FaceClassificationRegistration
 #rm -f ../share/openbr/models/features/FaceClassificationExtraction
-rm -f ../share/openbr/models/algorithms/GenderClassification
+#rm -f ../share/openbr/models/algorithms/GenderClassification
 
-br -algorithm GenderClassification -path ../data/PCSO/Images -train "../data/PCSO/PCSO.db[query='SELECT File,Gender,PersonID FROM PCSO', subset=0:8000]" ../share/openbr/models/algorithms/GenderClassification
+export BR=../build/app/br/br
+export genderAlg=GenderClassification
+
+export PCSO_DIR=/user/pripshare/Databases/FaceDatabases/PCSO/PCSO/
+
+$BR -useGui 0  -algorithm $genderAlg -path $PCSO_DIR/Images -train "$PCSO_DIR/PCSO.db[query='SELECT File,Gender,PersonID FROM PCSO', subset=0:8000]" ../share/openbr/models/algorithms/GenderClassification
