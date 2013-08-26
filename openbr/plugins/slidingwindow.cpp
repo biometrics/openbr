63d945ada71d08718f641e515876c6976afdc1ca
Austin Van Blanton <imaus10@gmail.com>
2013-08-26 14:56:03 -0400

    Put transform first for easy default usage

--- openbr/plugins/slidingwindow.cpp
+++ openbr/plugins/slidingwindow.cpp
@@ -16,19 +16,19 @@ namespace br
 class SlidingWindowTransform : public Transform
 {
     Q_OBJECT
+    Q_PROPERTY(br::Transform *transform READ get_transform WRITE set_transform RESET reset_transform STORED false)
     Q_PROPERTY(int minSize READ get_minSize WRITE set_minSize RESET reset_minSize STORED false)
     Q_PROPERTY(double scaleFactor READ get_scaleFactor WRITE set_scaleFactor RESET reset_scaleFactor STORED false)
     Q_PROPERTY(double stepSize READ get_stepSize WRITE set_stepSize RESET reset_stepSize STORED false)
     Q_PROPERTY(bool takeLargestScale READ get_takeLargestScale WRITE set_takeLargestScale RESET reset_takeLargestScale STORED false)
-    Q_PROPERTY(br::Transform *transform READ get_transform WRITE set_transform RESET reset_transform STORED false)
     Q_PROPERTY(bool negSamples READ get_negSamples WRITE set_negSamples RESET reset_negSamples STORED false)
     Q_PROPERTY(int negToPosRatio READ get_negToPosRatio WRITE set_negToPosRatio RESET reset_negToPosRatio STORED false)
     Q_PROPERTY(double maxOverlap READ get_maxOverlap WRITE set_maxOverlap RESET reset_maxOverlap STORED false)
+    BR_PROPERTY(br::Transform *, transform, NULL)
     BR_PROPERTY(int, minSize, 8)
     BR_PROPERTY(double, scaleFactor, 0.75)
     BR_PROPERTY(double, stepSize, 1)
     BR_PROPERTY(bool, takeLargestScale, true)
-    BR_PROPERTY(br::Transform *, transform, NULL)
     BR_PROPERTY(bool, negSamples, true)
     BR_PROPERTY(int, negToPosRatio, 1)
     BR_PROPERTY(double, maxOverlap, 0)
