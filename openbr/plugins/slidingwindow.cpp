d4b9e953370b65ed90d38938d5ae7da5de311f9a
Brendan Klare <brendan.klare@noblis.org>
2013-10-16 10:01:55 -0400

    Scale normalization for sliding window

--- openbr/plugins/slidingwindow.cpp
+++ openbr/plugins/slidingwindow.cpp
@@ -3,6 +3,8 @@
 #include "openbr/core/common.h"
 #include "openbr/core/qtutils.h"
 #include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 using namespace cv;
 
@@ -21,26 +23,28 @@ class SlidingWindowTransform : public Transform
     Q_PROPERTY(br::Transform *transform READ get_transform WRITE set_transform RESET reset_transform STORED false)
     Q_PROPERTY(int minSize READ get_minSize WRITE set_minSize RESET reset_minSize STORED false)
     Q_PROPERTY(double scaleFactor READ get_scaleFactor WRITE set_scaleFactor RESET reset_scaleFactor STORED false)
-    Q_PROPERTY(double stepSize READ get_stepSize WRITE set_stepSize RESET reset_stepSize STORED false)
+    Q_PROPERTY(int stepSize READ get_stepSize WRITE set_stepSize RESET reset_stepSize STORED false)
     Q_PROPERTY(bool takeLargestScale READ get_takeLargestScale WRITE set_takeLargestScale RESET reset_takeLargestScale STORED false)
     Q_PROPERTY(bool negSamples READ get_negSamples WRITE set_negSamples RESET reset_negSamples STORED false)
     Q_PROPERTY(int negToPosRatio READ get_negToPosRatio WRITE set_negToPosRatio RESET reset_negToPosRatio STORED false)
     Q_PROPERTY(double maxOverlap READ get_maxOverlap WRITE set_maxOverlap RESET reset_maxOverlap STORED false)
     Q_PROPERTY(double aspectRatio READ get_aspectRatio WRITE set_aspectRatio RESET reset_aspectRatio STORED true)
+    Q_PROPERTY(int windowWidth READ get_windowWidth WRITE set_windowWidth RESET reset_windowWidth STORED false)
     BR_PROPERTY(br::Transform *, transform, NULL)
     BR_PROPERTY(int, minSize, 8)
     BR_PROPERTY(double, scaleFactor, 0.75)
-    BR_PROPERTY(double, stepSize, 1)
+    BR_PROPERTY(int, stepSize, 1)
     BR_PROPERTY(bool, takeLargestScale, true)
     BR_PROPERTY(bool, negSamples, true)
     BR_PROPERTY(int, negToPosRatio, 1)
     BR_PROPERTY(double, maxOverlap, 0)
     BR_PROPERTY(double, aspectRatio, 1)
+    BR_PROPERTY(int, windowWidth, 24)
 
 public:
     SlidingWindowTransform() : Transform(false, true) {}
-
 private:
+
     void train(const TemplateList &data)
     {
         if (transform->trainable) {
@@ -55,13 +59,15 @@ private:
                         continue;
                     }
 
-                    QString buffer;
+                    //learn aspect ratio
+                    double aspectRatio = (double)posRect.width / (double)posRect.height;
+                    tempRatio += aspectRatio;
+                    ratioCnt += 1;
+
+                    Mat scaledImg;
+                    resize(Mat(tmpl, posRect), scaledImg, Size(windowWidth,round(windowWidth / aspectRatio)));
                     Template pos(tmpl.file, Mat(tmpl, posRect));
                     full += pos;
-
-                    //learn aspect ratio
-                    tempRatio += ((double)posRect.width / (double)posRect.height);
-                    ratioCnt += 1;
 
                     // add random negative samples
                     if (negSamples) {
@@ -109,18 +115,28 @@ private:
         if (src.file.getBool("Train", false)) return;
 
         dst.file.clearRects();
-        int rows = src.m().rows, cols = src.m().cols;
-        for (double size=std::min(rows, cols); size>=minSize; size*=scaleFactor) {
-            double h = size / aspectRatio;
-            for (double y=0; y+h<rows; y+=(size*stepSize)) {
-                for (double x=0; x+size<cols; x+=(size*stepSize)) {
-                    Rect window(x, y, size, h);
-                    Template windowMat(src.file, Mat(src.m(), window));
+        int rows = src.m().rows;
+        int cols = src.m().cols;
+        int windowHeight = (int) round((float) windowWidth / aspectRatio);
+        float startScale;
+        if ((cols / rows) > aspectRatio)
+            startScale = round((float) rows / (float) windowHeight);
+        else
+            startScale = round((float) cols / (float) windowWidth);
+
+        for (float scale = startScale; scale >= 1.0; scale -= (1.0 - scaleFactor)) {
+            Mat scaleImg;
+            resize(src, scaleImg, Size(round(cols / scale), round(rows / scale)));
+
+            for (double y = 0; y + windowHeight < scaleImg.rows; y += stepSize) {
+                for (double x = 0; x + windowWidth < scaleImg.cols; x += stepSize) {
+                    Rect window(x, y, windowWidth, windowHeight);
+                    Template windowMat(src.file, Mat(scaleImg, window));
                     Template detect;
                     transform->project(windowMat, detect);
                     // the result will be in the Label
                     if (detect.file.get<QString>("Label") == "pos") {
-                        dst.file.appendRect(OpenCVUtils::fromRect(window));
+                        dst.file.appendRect(QRectF((float) x * scale, (float) y * scale, (float) windowWidth * scale, (float) windowHeight * scale));
                         float confidence = detect.file.get<float>("Dist");
                         QList<float> confidences = dst.file.getList<float>("Confidences", QList<float>());
                         confidences.append(confidence);
