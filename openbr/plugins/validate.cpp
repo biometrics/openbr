2cc56616e6c86002ab161f196e17e4901ca71dd4
sklum <scott.klum@gmail.com>
2013-10-23 16:54:07 -0700

    Restored filter to origin state

--- openbr/plugins/validate.cpp
+++ openbr/plugins/validate.cpp
@@ -166,26 +166,21 @@ class FilterDistance : public Distance
 
     float compare(const Template &a, const Template &b) const
     {
-        Q_OBJECT
-
-        float compare(const Template &a, const Template &b) const
-        {
-            (void) b; // Query template isn't checked
-            foreach (const QString &key, Globals->filters.keys()) {
-                bool keep = false;
-                const QString metadata = a.file.get<QString>(key, "");
-                if (Globals->filters[key].isEmpty()) continue;
-                if (metadata.isEmpty()) return -std::numeric_limits<float>::max();
-                foreach (const QString &value, Globals->filters[key]) {
-                    if (metadata == value) {
-                        keep = true;
-                        break;
-                    }
+        (void) b; // Query template isn't checked
+        foreach (const QString &key, Globals->filters.keys()) {
+            bool keep = false;
+            const QString metadata = a.file.get<QString>(key, "");
+            if (Globals->filters[key].isEmpty()) continue;
+            if (metadata.isEmpty()) return -std::numeric_limits<float>::max();
+            foreach (const QString &value, Globals->filters[key]) {
+                if (metadata == value) {
+                    keep = true;
+                    break;
                 }
-                if (!keep) return -std::numeric_limits<float>::max();
             }
-            return 0;
+            if (!keep) return -std::numeric_limits<float>::max();
         }
+        return 0;
     }
 };
 
