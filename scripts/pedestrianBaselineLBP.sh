f82644f113223e578d657be2e323932eeebcd2af
Brendan Klare <bklare@gmail.com>
2013-11-14 10:18:03 -0500

    Updated pedestrian baseline alg

--- scripts/pedestrianBaselineLBP.sh
+++ scripts/pedestrianBaselineLBP.sh
@@ -6,8 +6,10 @@
 #Make sure you set your data path. This will likely by your openbr/data directory.
 INRIA_PATH=$DATA/INRIAPerson
 
+ALG="Open+Cvt(Gray)+Rename(neg,0)+BuildScales(Blur(2)+LBP(1,2)+SlidingWindow(Hist(59)+Cat+LDA(isBinary=true),windowWidth=10,takeLargestScale=false,threshold=2),windowWidth=10,takeLargestScale=false,minScale=4)+ConsolidateDetections+Discard"
+
 br -useGui 0 \
-    -algorithm "Open+Cvt(Gray)+Rename(neg,0)+BuildScales(Blur(2)+LBP(1,2)+SlidingWindow(Hist(59)+Cat+LDA(isBinary=true),windowWidth=10,takeLargestScale=false,threshold=3),windowWidth=10,takeLargestScale=false,minScale=4)+Discard" \
+   -algorithm "${ALG}" \
    -path $INRIA_PATH/img \
    -train $INRIA_PATH/sigset/train.xml pedModel \
    -enroll $INRIA_PATH/sigset/testSmall.xml pedResults.xml
