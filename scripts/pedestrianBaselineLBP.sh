b0ac58012e69a9c0e351c4bdb188821e40e16c31
Austin Blanton <imaus10@gmail.com>
2013-11-15 11:18:21 -0500

    Plot detection results

--- scripts/pedestrianBaselineLBP.sh
+++ scripts/pedestrianBaselineLBP.sh
@@ -10,18 +10,21 @@ else
     INRIA_PATH=$DATA/INRIAPerson
 fi
 
-ALG="Open+Cvt(Gray)+Rename(neg,0)+BuildScales(Blur(2)+LBP(1,2)+SlidingWindow(Hist(59)+Cat+LDA(isBinary=true),windowWidth=10,takeLargestScale=false,threshold=2),windowWidth=10,takeLargestScale=false,minScale=4)+ConsolidateDetections+Discard"
+ALG="Open+Cvt(Gray)+BuildScales(Blur(2)+LBP(1,2)+SlidingWindow(Hist(59)+Cat+LDA(isBinary=true),windowWidth=10,takeLargestScale=false,threshold=2),windowWidth=10,takeLargestScale=false,minScale=4)+Discard"
 
 # Josh's new algorithm (in progress)
 # ALG="Open+Cvt(Gray)+Detector(Gradient+Bin(0,360,9,true)+Merge+Integral+IntegralSlidingWindow(RecursiveIntegralSampler(2,2,0,PCA(0.95))+Cat+LDA(0.95,isBinary=true)))"
 
+#TEST=testSmall.xml
+TEST=test.xml
+
 br -useGui 0 \
    -algorithm "${ALG}" \
    -path $INRIA_PATH/img \
-   -train $INRIA_PATH/sigset/train.xml pedModel
+   -train $INRIA_PATH/sigset/train.xml pedModel \
+   -enroll $INRIA_PATH/sigset/$TEST pedResults.xml
 
-br -algorithm pedModel \
-   -path $INRIA_PATH/img \
-   -enroll $INRIA_PATH/sigset/testSmall.xml pedResults.xml
+br -evalDetection pedResults.xml $INRIA_PATH/sigset/$TEST pedEvalResults.csv \
+   -plotDetection pedEvalResults.csv pedPlots.pdf
 
-br -parallelism 0 -algorithm Open+Draw+Show -path $INRIA_PATH/img -enroll pedResults.xml
+#br -parallelism 0 -algorithm Open+Draw+Show -path $INRIA_PATH/img -enroll pedResults.xml
