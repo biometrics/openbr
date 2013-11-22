9309ed63daca130c4f46c59b5e19d760e4dae14c
Josh Klontz <Joshua.Klontz@noblis.org>
2013-11-22 10:04:17 -0500

    generalized evalGenderRecognition script

--- scripts/evalGenderClassification-PCSO.sh
+++ scripts/evalGenderClassification-PCSO.sh
@@ -5,12 +5,11 @@ if [ ! -f evalGenderClassification-PCSO.sh ]; then
 fi
 
 export BR=../build/app/br/br
-export genderAlg=GenderClassification
-
-export PCSO_DIR=/user/pripshare/Databases/FaceDatabases/PCSO/PCSO/
+export ALGORITHM=GenderClassification
+export PCSO_DIR=../data/PCSO/img
 
 # Create a file list by querying the database
 $BR -useGui 0 -quiet -algorithm Identity -enroll "$PCSO_DIR/PCSO.db[query='SELECT File,Gender,PersonID FROM PCSO', subset=1:8000]" terminal.txt > Input.txt
 
 # Enroll the file list and evaluate performance
-$BR -useGui 0  -algorithm $genderAlg -path $PCSO_DIR/Images -enroll Input.txt Output.txt -evalClassification Output.txt Input.txt Gender
+$BR -useGui 0  -algorithm $ALGORITHM -path $PCSO_DIR -enroll Input.txt Output.txt -evalClassification Output.txt Input.txt Gender
