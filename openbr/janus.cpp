5dad9e2336c4bf37b1f56485690f8a082e3e1e0a
Josh Klontz <Joshua.Klontz@noblis.org>
2013-12-02 13:41:42 -0500

    janus tweaks

--- openbr/janus.cpp
+++ openbr/janus.cpp
@@ -19,28 +19,29 @@ janus_error janus_initialize(const char *sdk_path, const char *model_file)
     return JANUS_SUCCESS;
 }
 
-void janus_finalize()
+janus_error janus_finalize()
 {
     br::Context::finalize();
-}
-
-janus_error janus_initialize_template(janus_partial_template *partial_template)
-{
-    (void) partial_template;
     return JANUS_SUCCESS;
 }
 
-janus_error janus_add_image(const janus_image image, const janus_attribute_list attributes, janus_partial_template partial_template)
+janus_error janus_initialize_template(janus_incomplete_template *incomplete_template)
+{
+    (void) incomplete_template;
+    return JANUS_SUCCESS;
+}
+
+janus_error janus_add_image(const janus_image image, const janus_attribute_list attributes, janus_incomplete_template incomplete_template)
 {
     (void) image;
     (void) attributes;
-    (void) partial_template;
+    (void) incomplete_template;
     return JANUS_SUCCESS;
 }
 
-janus_error janus_finalize_template(janus_partial_template partial_template, janus_template template_, size_t *bytes)
+janus_error janus_finalize_template(janus_incomplete_template incomplete_template, janus_template template_, size_t *bytes)
 {
-    (void) partial_template;
+    (void) incomplete_template;
     (void) template_;
     *bytes = 0;
     return JANUS_SUCCESS;
