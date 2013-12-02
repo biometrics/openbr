9f2112e74ad55715754a79b0212954f552135107
Josh Klontz <Joshua.Klontz@noblis.org>
2013-12-02 12:41:43 -0500

    added placeholder janus functions

--- openbr/janus.cpp
+++ openbr/janus.cpp
@@ -23,3 +23,35 @@ void janus_finalize()
 {
     br::Context::finalize();
 }
+
+janus_error janus_initialize_template(janus_partial_template *partial_template)
+{
+    (void) partial_template;
+    return JANUS_SUCCESS;
+}
+
+janus_error janus_add_image(const janus_image image, const janus_attribute_list attributes, janus_partial_template partial_template)
+{
+    (void) image;
+    (void) attributes;
+    (void) partial_template;
+    return JANUS_SUCCESS;
+}
+
+janus_error janus_finalize_template(janus_partial_template partial_template, janus_template template_, size_t *bytes)
+{
+    (void) partial_template;
+    (void) template_;
+    *bytes = 0;
+    return JANUS_SUCCESS;
+}
+
+janus_error janus_verify(const janus_template a, const size_t a_bytes, const janus_template b, const size_t b_bytes, float *similarity)
+{
+    (void) a;
+    (void) a_bytes;
+    (void) b;
+    (void) b_bytes;
+    *similarity = 0;
+    return JANUS_SUCCESS;
+}
