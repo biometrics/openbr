cee70e6a2225f339a4660ccc41f76e48a9aca4dc
Austin Blanton <imaus10@gmail.com>
2014-02-05 21:03:41 -0500

    install cvmatio libs (well, only for mac...)

--- share/openbr/cmake/Findcvmatio.cmake
+++ share/openbr/cmake/Findcvmatio.cmake
@@ -9,5 +9,11 @@ endif()
 add_subdirectory(${CVMATIO_DIR})
 include_directories(${CVMATIO_DIR}/include)
 set(BR_THIRDPARTY_SRC ${BR_THIRDPARTY_SRC} ${CVMATIO_DIR}/src/MatlabIO.cpp)
-link_directories(${CMAKE_BINARY_DIR}/3rdparty/cvmatio/src)
+set(CVMATIO_LIB_DIR ${CMAKE_BINARY_DIR}/3rdparty/cvmatio/src)
+link_directories(${CVMATIO_LIB_DIR})
 set(BR_THIRDPARTY_LIBS ${BR_THIRDPARTY_LIBS} cvmatio)
+# because the project's cmake installs to the project folder,
+# install(TARGETS cvmatio DESTINATION lib) doesn't work
+# (i get error "target doesn't exist in this directory")
+# so yeah, this won't work in windows
+install(FILES ${CVMATIO_LIB_DIR}/libcvmatio.dylib DESTINATION lib)
