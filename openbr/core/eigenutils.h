3d060770285e75890db520dcb327de1da525695c
Brendan Klare <brendan.klare@noblis.org>
2014-02-08 13:54:27 -0500

    Eigen debug helper functions

--- openbr/core/eigenutils.h
+++ openbr/core/eigenutils.h
@@ -26,6 +26,8 @@ void writeEigen(Eigen::MatrixXd X, QString filename);
 void writeEigen(Eigen::VectorXd X, QString filename);
 void writeEigen(Eigen::VectorXf X, QString filename);
 void printEigen(Eigen::MatrixXd X);
+void printEigen(Eigen::MatrixXf X);
+void printSize(Eigen::MatrixXf X);
 
 template<typename _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
 inline QDataStream &operator<<(QDataStream &stream, const Eigen::Matrix< _Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols > &mat)
