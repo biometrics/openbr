6f85ca66334ac85fed5582fc6a636a1ca635c8d1
Austin Blanton <imaus10@gmail.com>
2014-02-10 16:01:55 -0500

    Use cvmatio original repo after merge of pull request

--- share/openbr/cmake/Findcvmatio.cmake
+++ share/openbr/cmake/Findcvmatio.cmake
@@ -1,7 +1,7 @@
 set(CVMATIO_DIR "${BR_THIRDPARTY_DIR}/cvmatio")
 if(NOT EXISTS ${CVMATIO_DIR})
   # download source from github
-  execute_process(COMMAND "git" "clone" "https://github.com/biometrics/cvmatio.git" WORKING_DIRECTORY ${BR_THIRDPARTY_DIR})
+  execute_process(COMMAND "git" "clone" "https://github.com/hbristow/cvmatio.git" WORKING_DIRECTORY ${BR_THIRDPARTY_DIR})
 else()
   # update the source
   execute_process(COMMAND "git" "pull" WORKING_DIRECTORY ${CVMATIO_DIR})
