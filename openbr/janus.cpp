d3fcafb512d67521fda098410a1ddfbb4f22dc36
Josh Klontz <Joshua.Klontz@noblis.org>
2014-02-19 14:35:25 -0500

    janus fixes

--- openbr/janus.cpp
+++ openbr/janus.cpp
@@ -3,15 +3,18 @@
 #endif
 
 #include "janus.h"
+#include "janus_io.h"
 #include "openbr_plugin.h"
-
-// Use the provided default implementation of some functions
-#include "janus/src/janus.cpp"
 
 using namespace br;
 
 static QSharedPointer<Transform> transform;
 static QSharedPointer<Distance> distance;
+
+size_t janus_max_template_size()
+{
+    return JANUS_MAX_TEMPLATE_SIZE_LIMIT;
+}
 
 janus_error janus_initialize(const char *sdk_path, const char *model_file)
 {
@@ -19,9 +22,13 @@ janus_error janus_initialize(const char *sdk_path, const char *model_file)
     const char *argv[1] = { "janus" };
     Context::initialize(argc, (char**)argv, sdk_path);
     QString algorithm = model_file;
-    if (algorithm.isEmpty()) algorithm = "Cvt(Gray)+Affine(88,88,0.25,0.35)+<FaceRecognitionExtraction>+<FaceRecognitionEmbedding>+<FaceRecognitionQuantization>:ByteL1";
-    transform = Transform::fromAlgorithm(algorithm, false);
-    distance = Distance::fromAlgorithm(algorithm);
+    if (algorithm.isEmpty()) {
+        transform = Transform::fromAlgorithm("Cvt(Gray)+Affine(88,88,0.25,0.35)+<FaceRecognitionExtraction>+<FaceRecognitionEmbedding>+<FaceRecognitionQuantization>", false);
+        distance = Distance::fromAlgorithm("FaceRecognition");
+    } else {
+        transform = Transform::fromAlgorithm(algorithm, false);
+        distance = Distance::fromAlgorithm(algorithm);
+    }
     return JANUS_SUCCESS;
 }
 
@@ -40,24 +47,24 @@ janus_error janus_initialize_template(janus_template *template_)
     return JANUS_SUCCESS;
 }
 
-janus_error janus_add_image(const janus_image image, const janus_attribute_list attributes, janus_template template_)
+janus_error janus_augment(const janus_image image, const janus_attribute_list attributes, janus_template template_)
 {
     Template t;
     t.append(cv::Mat(image.height,
                      image.width,
-                     image.color_space == JANUS_GRAY8 ? CV_8UC1 : CV_8UC1,
+                     image.color_space == JANUS_GRAY8 ? CV_8UC1 : CV_8UC3,
                      image.data));
     for (size_t i=0; i<attributes.size; i++)
         t.file.set(janus_attribute_to_string(attributes.attributes[i]), attributes.values[i]);
 
-    if (!t.file.contains("JANUS_RIGHT_EYE_X") ||
-        !t.file.contains("JANUS_RIGHT_EYE_Y") ||
-        !t.file.contains("JANUS_LEFT_EYE_X") ||
-        !t.file.contains("JANUS_LEFT_EYE_Y"))
+    if (!t.file.contains("RIGHT_EYE_X") ||
+        !t.file.contains("RIGHT_EYE_Y") ||
+        !t.file.contains("LEFT_EYE_X") ||
+        !t.file.contains("LEFT_EYE_Y"))
         return JANUS_SUCCESS;
 
-    t.file.set("Affine_0", QPointF(t.file.get<float>("JANUS_RIGHT_EYE_X"), t.file.get<float>("JANUS_RIGHT_EYE_Y")));
-    t.file.set("Affine_1", QPointF(t.file.get<float>("JANUS_LEFT_EYE_X"), t.file.get<float>("JANUS_LEFT_EYE_Y")));
+    t.file.set("Affine_0", QPointF(t.file.get<float>("RIGHT_EYE_X"), t.file.get<float>("RIGHT_EYE_Y")));
+    t.file.set("Affine_1", QPointF(t.file.get<float>("LEFT_EYE_X"), t.file.get<float>("LEFT_EYE_Y")));
     Template u;
     transform->project(t, u);
     template_->append(u);
@@ -102,42 +109,35 @@ janus_error janus_verify(const janus_flat_template a, const size_t a_bytes, cons
     a_templates = *(reinterpret_cast<size_t*>(a)+1);
     b_template_bytes = *(reinterpret_cast<size_t*>(b)+0);
     b_templates = *(reinterpret_cast<size_t*>(b)+1);
+
+    *similarity = 0;
+    if ((a_templates == 0) || (b_templates == 0))
+        return JANUS_SUCCESS;
+
     if (a_template_bytes != b_template_bytes)
         return JANUS_UNKNOWN_ERROR;
 
-    float dist = 0;
     for (size_t i=0; i<a_templates; i++)
         for (size_t j=0; j<b_templates; j++)
-            dist += distance->compare(cv::Mat(1, a_template_bytes, CV_8UC1, a+2*sizeof(size_t)+i*a_template_bytes),
-                                      cv::Mat(1, b_template_bytes, CV_8UC1, b+2*sizeof(size_t)+i*b_template_bytes));
-    *similarity = a_templates * b_templates / dist;
-    return JANUS_SUCCESS;
-}
+            *similarity += distance->compare(cv::Mat(1, a_template_bytes, CV_8UC1, a+2*sizeof(size_t)+i*a_template_bytes),
+                                             cv::Mat(1, b_template_bytes, CV_8UC1, b+2*sizeof(size_t)+j*b_template_bytes));
 
-struct janus_gallery_type : public TemplateList
-{};
+    if (*similarity != *similarity) // True for NaN
+        return JANUS_UNKNOWN_ERROR;
 
-janus_error janus_initialize_gallery(janus_gallery *gallery)
-{
-    *gallery = new janus_gallery_type();
+    *similarity /= a_templates * b_templates;
     return JANUS_SUCCESS;
 }
 
 janus_error janus_enroll(const janus_template template_, const janus_template_id template_id, janus_gallery gallery)
 {
-    template_->file.set("Template_ID", template_id);
-    gallery->append(*template_);
-    delete template_;
-    return JANUS_SUCCESS;
-}
-
-janus_error janus_finalize_gallery(janus_gallery gallery, janus_gallery_file gallery_file)
-{
-    QFile file(gallery_file);
-    if (!file.open(QFile::WriteOnly))
+    template_->file.set("TEMPLATE_ID", template_id);
+    QFile file(gallery);
+    if (!file.open(QFile::WriteOnly | QFile::Append))
         return JANUS_WRITE_ERROR;
     QDataStream stream(&file);
-    stream << gallery;
+    stream << *template_;
     file.close();
+    delete template_;
     return JANUS_SUCCESS;
 }
