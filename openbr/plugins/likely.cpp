b5a06d643c7e3ad10a96a96e95622f0b45420a60
Josh Klontz <Joshua.Klontz@noblis.org>
2014-03-12 10:04:43 -0400

    working draft of Likely plugins

--- openbr/plugins/likely.cpp
+++ openbr/plugins/likely.cpp
@@ -1,10 +1,18 @@
 #include <likely.h>
+#include <likely/opencv.hpp>
 
 #include "openbr_internal.h"
 
 namespace br
 {
 
+/*!
+ * \ingroup transforms
+ * \brief Generic interface to Likely JIT compiler
+ *
+ * www.liblikely.org
+ * \author Josh Klontz \cite jklontz
+ */
 class LikelyTransform : public UntrainableTransform
 {
     Q_OBJECT
@@ -15,7 +23,7 @@ class LikelyTransform : public UntrainableTransform
 
     void init()
     {
-        likely_ast ast = likely_asts_from_string(qPrintable(kernel));
+        likely_ast ast = likely_ast_from_string(qPrintable(kernel));
         likely_env env = likely_new_env();
         function = likely_compile(ast, env, likely_type_null);
         likely_release_env(env);
@@ -24,12 +32,45 @@ class LikelyTransform : public UntrainableTransform
 
     void project(const Template &src, Template &dst) const
     {
-        dst = src;
+        likely_const_mat srcl = likely::fromCvMat(src);
+        likely_const_mat dstl = function(srcl);
+        dst = likely::toCvMat(dstl);
+        likely_release(dstl);
+        likely_release(srcl);
     }
 };
 
 BR_REGISTER(Transform, LikelyTransform)
 
-}
+/*!
+ * \ingroup formats
+ * \brief Likely matrix format
+ *
+ * www.liblikely.org
+ * \author Josh Klontz \cite jklontz
+ */
+class lmFormat : public Format
+{
+    Q_OBJECT
+
+    Template read() const
+    {
+        likely_const_mat m = likely_read(qPrintable(file.name));
+        Template result(likely::toCvMat(m));
+        likely_release(m);
+        return result;
+    }
+
+    void write(const Template &t) const
+    {
+        likely_const_mat m = likely::fromCvMat(t);
+        likely_write(m, qPrintable(file.name));
+        likely_release(m);
+    }
+};
+
+BR_REGISTER(Format, lmFormat)
+
+} // namespace br
 
 #include "likely.moc"
