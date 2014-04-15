d022425a57b4ff7c8bb3d8cb8c0af343276307b7
Josh Klontz <Joshua.Klontz@noblis.org>
2014-04-15 10:21:47 -0400

    added ExtractMetadataTransform

--- openbr/plugins/misc.cpp
+++ openbr/plugins/misc.cpp
@@ -366,6 +366,29 @@ BR_REGISTER(Transform, RegexPropertyTransform)
 
 /*!
  * \ingroup transforms
+ * \brief Create matrix from metadata values.
+ * \author Josh Klontz \cite jklontz
+ */
+class ExtractMetadataTransform : public UntrainableMetaTransform
+{
+    Q_OBJECT
+    Q_PROPERTY(QStringList keys READ get_keys WRITE set_keys RESET reset_keys STORED false)
+    BR_PROPERTY(QStringList, keys, QStringList())
+
+    void project(const Template &src, Template &dst) const
+    {
+        dst.file = src.file;
+        QList<float> values;
+        foreach (const QString &key, keys)
+            values.append(src.file.get<float>(key));
+        dst.append(OpenCVUtils::toMat(values, 1));
+    }
+};
+
+BR_REGISTER(Transform, ExtractMetadataTransform)
+
+/*!
+ * \ingroup transforms
  * \brief Store the last matrix of the input template as a metadata key with input property name.
  * \author Charles Otto \cite caotto
  */
