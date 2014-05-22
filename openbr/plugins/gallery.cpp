e544b45abada4360b82c541730326c8e27212940
Tunde <ogunfemi.b@gmail.com>
2014-05-22 16:46:12 -0400

    Fixed using tabs instead of spaces, and added myself to the bib file

--- openbr/plugins/gallery.cpp
+++ openbr/plugins/gallery.cpp
@@ -1033,47 +1033,47 @@ class topPredictionsGallery : public Gallery
     Q_PROPERTY(float out_count READ get_out_count WRITE set_out_count RESET reset_out_count STORED false)
     BR_PROPERTY(float, out_count, 3)
 
-	QMap<float, QString> attributes;
+    QMap<float, QString> attributes;
 
     ~topPredictionsGallery()
     {
-		QString result = QString("# of attributes: %1").arg(QString::number(attributes.count()));
+    	QString result = QString("# of attributes: %1").arg(QString::number(attributes.count()));
         QtUtils::writeFile(file.name, result);
         if (!attributes.isEmpty())
-		{
-			QMapIterator <float, QString> it(attributes);
-			it.toBack();
-			if (out_count > 1)
-			{
-				if (attributes.count() < out_count)
-					out_count = attributes.count();
-				
-				for (int i = 0; i < out_count; i++)
-				{
-					it.previous();
-					result = QString("%1)	%2 : %3").arg(QString::number(i + 1), it.value(), QString::number((float)it.key())); 
-            		QtUtils::writeFile(file.name, result);
-				}
-			}
-			else
-			{
-				int count = 0;
-				it.toBack();
-				while (it.hasPrevious())
-				{
-					it.previous();
-					if (it.key() >= out_count)
-					{
-						result = QString("%1)	%2 : %3").arg(QString::number(count + 1), it.value(), QString::number((float)it.key())); 
-            			QtUtils::writeFile(file.name, result);
-						count++;
-					}
-				}
-			}
-		}
+    	{
+    		QMapIterator <float, QString> it(attributes);
+    		it.toBack();
+    		if (out_count > 1)
+    		{
+    			if (attributes.count() < out_count)
+    				out_count = attributes.count();
+    			
+    			for (int i = 0; i < out_count; i++)
+    			{
+    				it.previous();
+    				result = QString("%1)	%2 : %3").arg(QString::number(i + 1), it.value(), QString::number((float)it.key())); 
+                	QtUtils::writeFile(file.name, result);
+    			}
+    		}
+    		else
+    		{
+    			int count = 0;
+    			it.toBack();
+    			while (it.hasPrevious())
+    			{
+    				it.previous();
+    				if (it.key() >= out_count)
+    				{
+    					result = QString("%1)	%2 : %3").arg(QString::number(count + 1), it.value(), QString::number((float)it.key())); 
+                		QtUtils::writeFile(file.name, result);
+    					count++;
+    				}
+    			}
+    		}
+    	}
     }
 
-	//Pure virtial function so needs an implementation
+    //Pure virtial function so needs an implementation
     TemplateList readBlock(bool *done)
     {
         *done = true;
@@ -1082,17 +1082,17 @@ class topPredictionsGallery : public Gallery
 
     void write(const Template &t)
     {
-		QList<QString> keys = t.file.localKeys();
-		for (int i = 0; i < keys.count(); i++)
-		{
-			QString key = keys.at(i);
-			if (key.startsWith("predicted_"))
-			{
-				float val = t.file.get<float>(key);
-				attributes.insert(val, key); //use float as key to keep in order
-			}
+    	QList<QString> keys = t.file.localKeys();
+    	for (int i = 0; i < keys.count(); i++)
+    	{
+    		QString key = keys.at(i);
+    		if (key.startsWith("predicted_"))
+    		{
+    			float val = t.file.get<float>(key);
+    			attributes.insert(val, key); //use float as key to keep in order
+    		}
 
-		}
+    	}
     }
 };
 
