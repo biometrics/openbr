259b3639f3dc27cac53ac40b89f60ee0a0e94dc6
Josh Klontz <josh.klontz@gmail.com>
2014-06-13 10:21:59 -0400

    fixed windows compilation errors

--- openbr/plugins/gallery.cpp
+++ openbr/plugins/gallery.cpp
@@ -14,6 +14,7 @@
  * limitations under the License.                                            *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+#include <QCryptographicHash>
 #include <QRegularExpression>
 #include <QtConcurrentRun>
 #ifndef BR_EMBEDDED
