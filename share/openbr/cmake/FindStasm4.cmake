9926eba5b7de8141ac70f406be06c0c057951d4d
Scott Klum <scott.klum@gmail.com>
2014-08-19 10:40:50 -0400

    Fixed build, install

--- share/openbr/cmake/FindStasm4.cmake
+++ share/openbr/cmake/FindStasm4.cmake
@@ -7,14 +7,30 @@
 #   find_package(Stasm4 REQUIRED)
 #   target_link_libraries(MY_TARGET ${Stasm4_LIBS})
 # ================================================================
+include(ExternalProject)
+
+get_cmake_property(CACHE_VARS CACHE_VARIABLES)
+foreach(CACHE_VAR ${CACHE_VARS})
+  get_property(CACHE_VAR_HELPSTRING CACHE ${CACHE_VAR} PROPERTY HELPSTRING)
+  if(CACHE_VAR_HELPSTRING STREQUAL "No help, variable specified on the command line.")
+    get_property(CACHE_VAR_TYPE CACHE ${CACHE_VAR} PROPERTY TYPE)
+    if(CACHE_VAR_TYPE STREQUAL "UNINITIALIZED")
+      set(CACHE_VAR_TYPE)
+    else()
+      set(CACHE_VAR_TYPE :${CACHE_VAR_TYPE})
+    endif()
+    set(CMAKE_ARGS "${CMAKE_ARGS} -D${CACHE_VAR}${CACHE_VAR_TYPE}=\"${${CACHE_VAR}}\"")
+  endif()
+endforeach()
 
 find_path(Stasm_DIR stasm/stasm_lib.h ${CMAKE_SOURCE_DIR}/3rdparty/*)
 
-add_subdirectory(${Stasm_DIR} ${Stasm_DIR}/build)
+ExternalProject_Add(stasm
+  URL ${Stasm_DIR}
+  CMAKE_ARGS ${CMAKE_ARGS}
+  )
 
-set(SRC ${SOURCE};${SRC})
+ExternalProject_Get_Property(stasm install_dir)
+include_directories(${install_dir}/src/stasm-build/Stasm_DIR/build/install/include)
 
-include_directories(${Stasm_DIR}/stasm)
-link_directories(${Stasm_DIR}/build)
-
-set(Stasm4_LIBS stasm)
+file(GLOB Stasm4_LIBS ${install_dir}/src/stasm-build/Stasm_DIR/build/install/lib/*)
