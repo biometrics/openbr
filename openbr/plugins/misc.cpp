24147dd51cdf29dca4a3b8c4af0abf9bc71cde97
Brendan Klare <brendan.klare@noblis.org>
2014-08-22 11:40:19 -0400

    Add parameter for rename metadata key to keep original key

--- openbr/plugins/misc.cpp
+++ openbr/plugins/misc.cpp
@@ -376,15 +376,18 @@ class RenameTransform : public UntrainableMetadataTransform
     Q_OBJECT
     Q_PROPERTY(QString find READ get_find WRITE set_find RESET reset_find STORED false)
     Q_PROPERTY(QString replace READ get_replace WRITE set_replace RESET reset_replace STORED false)
+    Q_PROPERTY(bool keepOldKey READ get_keepOldKey WRITE set_keepOldKey RESET reset_keepOldKey STORED false)
     BR_PROPERTY(QString, find, "")
     BR_PROPERTY(QString, replace, "")
+    BR_PROPERTY(bool, keepOldKey, false)
 
     void projectMetadata(const File &src, File &dst) const
     {
         dst = src;
         if (dst.localKeys().contains(find)) {
             dst.set(replace, dst.value(find));
-            dst.remove(find);
+            if (!keepOldKey)
+               dst.remove(find);
         }
     }
 };
