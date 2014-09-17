dc5144c6ff31b27633c09b85121f31e0c6c70fd5
Scott Klum <scott.klum@gmail.com>
2014-09-17 19:34:54 -0400

    Added STASM 4.1.0 code

--- 3rdparty/stasm4.0.0/stasm/include/classic/README.txt
+++
@@ -1,3 +0,0 @@
-These are machine generated fitness function files for classical 1D descriptors.
-
-milbo berea feb 2013
---
+++ 3rdparty/stasm4.0.0/stasm/stasm/include/stasm.h
@@ -0,0 +1,21 @@
+// stasm.h: (nearly) all include files for the Stasm package
+//
+// Copyright (C) 2005-2013, Stephen Milborrow
+
+#ifndef STASM_H
+#define STASM_H
+
+const char* const stasm_VERSION = "4.1.0";
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+#include <string.h>
+#include <vector>
+#include <string>
+#include <functional>
+#include <algorithm>
+
+#endif // STASM_H
