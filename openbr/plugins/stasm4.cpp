12ed1cb782fdbb02ad10b4b3303048c9385334e8
Josh Klontz <josh.klontz@gmail.com>
2014-10-09 10:32:37 -0400

    Revert "Ugh"
    This reverts commit cd292c6ec4c13ac610cbdf4d219ab953b7c4fa1b.

--- openbr/plugins/stasm4.cpp
+++ openbr/plugins/stasm4.cpp
@@ -101,17 +101,16 @@ class StasmTransform : public UntrainableTransform
             QPointF rightEye;
             QPointF leftEye;
 
-            QString r = pinEyes.at(0).toString();
-            QString l = pinEyes.at(1).toString();
-
             if (src.file.contains("Affine_0") && src.file.contains("Affine_1")) {
-                rightEye = QtUtils::toPoint(r);
-                leftEye = QtUtils::toPoint(l);
+                rightEye = pinEyes.at(0).toPointF();
+                leftEye = pinEyes.at(1).toPointF();
                 if (!rightEye.isNull() && !leftEye.isNull())
                     ok = true;
             }
 
             if (!ok) {
+                QString r = pinEyes.at(0).toString();
+                QString l = pinEyes.at(1).toString();
                 if (!r.isNull() && !l.isNull() && src.file.contains(r) && src.file.contains(l))
                 {
                     rightEye = src.file.get<QPointF>(r, QPointF());
