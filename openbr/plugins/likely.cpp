5bd7cd9a98c265ff739e2db9fa9d79949e09b11e
Josh Klontz <josh.klontz@gmail.com>
2014-11-14 16:12:50 -0500

    first draft of lmat gallery

--- openbr/plugins/likely.cpp
+++ openbr/plugins/likely.cpp
@@ -1,6 +1,7 @@
 #include <likely.h>
 #include <likely/opencv.hpp>
 
+#include "openbr/core/opencvutils.h"
 #include "openbr_internal.h"
 
 namespace br
@@ -84,6 +85,39 @@ class lmatFormat : public Format
 };
 
 BR_REGISTER(Format, lmatFormat)
+
+/*!
+ * \ingroup formats
+ * \brief Likely matrix format
+ *
+ * www.liblikely.org
+ * \author Josh Klontz \cite jklontz
+ */
+class lmatGallery : public Gallery
+{
+    Q_OBJECT
+    QList<cv::Mat> mats;
+
+    ~lmatGallery()
+    {
+        const likely_const_mat m = likelyFromOpenCVMat(OpenCVUtils::toMatByRow(mats));
+        likely_write(m, qPrintable(file.name));
+        likely_release_mat(m);
+    }
+
+    TemplateList readBlock(bool *done)
+    {
+        *done = true;
+        qFatal("Not supported.");
+    }
+
+    void write(const Template &t)
+    {
+        mats.append(t);
+    }
+};
+
+BR_REGISTER(Gallery, lmatGallery)
 
 } // namespace br
 
