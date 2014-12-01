971dbf739384d9af4ddf3af07d960bf73e96cc7d
Josh Klontz <josh.klontz@gmail.com>
2014-12-01 09:43:21 -0500

    Introduced GroupTransform

--- openbr/plugins/regions.cpp
+++ openbr/plugins/regions.cpp
@@ -112,6 +112,45 @@ BR_REGISTER(Transform, CatTransform)
 
 /*!
  * \ingroup transforms
+ * \brief Group all input matrices into a single matrix.
+ *
+ * Similar to CatTransfrom but groups every _size_ adjacent matricies.
+ * \author Josh Klontz \cite jklontz
+ */
+class GroupTransform : public UntrainableMetaTransform
+{
+    Q_OBJECT
+    Q_PROPERTY(int size READ get_size WRITE set_size RESET reset_size)
+    BR_PROPERTY(int, size, 1)
+
+    void project(const Template &src, Template &dst) const
+    {
+        dst.file = src.file;
+
+        if (src.size() % size != 0)
+            qFatal("%d size does not evenly divide %d matrices.", size, src.size());
+        QVector<int> sizes(src.size() / size, 0);
+        for (int i=0; i<src.size(); i++)
+            sizes[i/size] += src[i].total();
+
+        if (!src.empty())
+            foreach (int size, sizes)
+                dst.append(Mat(1, size, src.m().type()));
+
+        QVector<int> offsets(src.size() / size, 0);
+        for (int i=0; i<src.size(); i++) {
+            size_t size = src[i].total() * src[i].elemSize();
+            int j = i / size;
+            memcpy(&dst[j].data[offsets[j]], src[i].ptr(), size);
+            offsets[j] += size;
+        }
+    }
+};
+
+BR_REGISTER(Transform, GroupTransform)
+
+/*!
+ * \ingroup transforms
  * \brief Concatenates all input matrices by row into a single matrix.
  * All matricies must have the same column counts.
  * \author Josh Klontz \cite jklontz
