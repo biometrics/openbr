5efddc3d0a1b06a9e1fc1f83c23ad03fe923edde
Scott Klum <scott.klum@gmail.com>
2014-12-29 13:03:18 -0500

    Added bool to control dup of landmarks

--- openbr/plugins/regions.cpp
+++ openbr/plugins/regions.cpp
@@ -242,19 +242,23 @@ class DupTransform : public UntrainableMetaTransform
 {
     Q_OBJECT
     Q_PROPERTY(int n READ get_n WRITE set_n RESET reset_n STORED false)
+    Q_PROPERTY(bool dupLandmarks READ get_dupLandmarks WRITE set_dupLandmarks RESET reset_dupLandmarks STORED false)
     BR_PROPERTY(int, n, 1)
+    BR_PROPERTY(bool, dupLandmarks, false)
 
     void project(const Template &src, Template &dst) const
     {
-        QList<QPointF> points = src.file.points();
-        QList<QRectF> rects = src.file.rects();
-
         for (int i=0; i<n; i++)
             dst.merge(src);
 
-        for (int i=1; i<n; i++) {
-            dst.file.appendPoints(points);
-            dst.file.appendRects(rects);
+        if (dupLandmarks) {
+            QList<QPointF> points = src.file.points();
+            QList<QRectF> rects = src.file.rects();
+
+            for (int i=1; i<n; i++) {
+                dst.file.appendPoints(points);
+                dst.file.appendRects(rects);
+            }
         }
     }
 };
