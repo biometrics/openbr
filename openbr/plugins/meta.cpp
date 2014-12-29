8f903c523ec14a9a86f477267002c23477db4175
Scott Klum <scott.klum@gmail.com>
2014-12-29 14:22:51 -0500

    Added Schrodinger

--- openbr/plugins/meta.cpp
+++ openbr/plugins/meta.cpp
@@ -783,6 +783,43 @@ public:
 };
 BR_REGISTER(Transform, DistributeTemplateTransform)
 
+/*!
+ * \ingroup transforms
+ * \brief Generates two templates, one of which is passed through a transform and the other
+ *        is not. No cats were harmed in the making of this transform.
+ * \author Scott Klum \cite sklum
+ */
+class SchrodingerTransform : public MetaTransform
+{
+    Q_OBJECT
+    Q_PROPERTY(br::Transform* transform READ get_transform WRITE set_transform RESET reset_transform)
+    BR_PROPERTY(br::Transform*, transform, NULL)
+
+public:
+    void train(const TemplateList &data)
+    {
+        transform->train(data);
+    }
+
+    void project(const TemplateList &src, TemplateList &dst) const
+    {
+        foreach(const Template &t, src) {
+            dst.append(t);
+            Template u;
+            transform->project(t,u);
+            dst.append(u);
+        }
+    }
+
+    void project(const Template &src, Template &dst) const {
+        TemplateList temp;
+        project(TemplateList() << src, temp);
+        if (!temp.isEmpty()) dst = temp.first();
+    }
+
+};
+BR_REGISTER(Transform, SchrodingerTransform)
+
 } // namespace br
 
 #include "meta.moc"
