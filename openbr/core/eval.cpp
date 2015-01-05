3ed08edc59866f07ee5fa4acb73effc796ce14ab
Ben Klein <benhunter1990@gmail.com>
2015-01-05 17:11:25 -0500

    Fix bug where non-existant .gal target file causes -eval to crash

--- openbr/core/eval.cpp
+++ openbr/core/eval.cpp
@@ -305,7 +305,7 @@ float Evaluate(const Mat &simmat, const Mat &mask, const QString &csv, const QSt
 
     // Attempt to read template size from enrolled gallery and write to output CSV
     size_t maxSize(0);
-    if (target.endsWith(".gal")) {
+    if (target.endsWith(".gal") && QFileInfo(target).exists()) {
         foreach (const Template &t, TemplateList::fromGallery(target)) maxSize = max(maxSize, t.bytes());
         lines.append(QString("TS,,%1").arg(QString::number(maxSize)));
     }
