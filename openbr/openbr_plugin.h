26f3043163e1175fbd0357a76ec0b33b639603fa
JordanCheney <m29803@1543338j-m29803.mitretek.org>
2015-01-21 16:49:50 -0500

    Change to threshold convention

--- openbr/openbr_plugin.h
+++ openbr/openbr_plugin.h
@@ -1401,7 +1401,9 @@ public:
 
     static Classifier *make(QString str, QObject *parent); /*!< \brief Make a classifier from a string. */
     virtual void train(const QList<cv::Mat> &images, const QList<float> &labels) = 0;
-    virtual float classify(const cv::Mat &image, bool returnSum = false) const = 0;
+    // By convention, classify should return a value normalized such that the threshold is 0. Negative values
+    // can be interpreted as negative samples while positives values are positive samples.
+    virtual float classify(const cv::Mat &image) const = 0;
 };
 
 /*!
