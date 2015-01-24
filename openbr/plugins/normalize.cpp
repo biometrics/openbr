a8eb67946b2c15ae3975cd8eee9430e5aba18c5a
Josh Klontz <josh.klontz@gmail.com>
2015-01-24 17:21:18 -0500

    introduced NormalizeTransform::squareRoot

--- openbr/plugins/normalize.cpp
+++ openbr/plugins/normalize.cpp
@@ -64,6 +64,8 @@ class NormalizeTransform : public UntrainableTransform
     BR_PROPERTY(int, alpha, 1)
     Q_PROPERTY(int beta READ get_beta WRITE set_beta RESET reset_beta STORED false)
     BR_PROPERTY(int, beta, 0)
+    Q_PROPERTY(bool squareRoot READ get_squareRoot WRITE set_squareRoot RESET reset_squareRoot STORED false)
+    BR_PROPERTY(bool, squareRoot, false)
 
 public:
     /*!< */
@@ -75,14 +77,30 @@ public:
 private:
     BR_PROPERTY(NormType, normType, L2)
 
+    static void signedSquareRoot(Mat &m)
+    {
+        for (int i=0; i<m.rows; i++)
+            for (int j=0; j<m.cols; j++) {
+                float &val = m.at<float>(i, j);
+                val = sqrtf(fabsf(val)) * (val >= 0 ? 1 : -1);
+            }
+    }
+
     void project(const Template &src, Template &dst) const
     {
-        if (!ByRow) normalize(src, dst, alpha, beta, normType, CV_32F);
+        if (!ByRow) {
+            normalize(src, dst, alpha, beta, normType, CV_32F);
+            if (squareRoot)
+                signedSquareRoot(dst);
+        }
+
         else {
             dst = src;
             for (int i=0; i<dst.m().rows; i++) {
                 Mat temp;
                 cv::normalize(dst.m().row(i), temp, alpha, beta, normType);
+                if (squareRoot)
+                    signedSquareRoot(temp);
                 temp.copyTo(dst.m().row(i));
             }
         }
