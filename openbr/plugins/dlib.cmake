240aef404179f7db90b558a5f483ee360ed8076c
Scott Klum <scott.klum@gmail.com>
2015-02-16 16:45:27 -0500

    Even better way to compile

--- openbr/plugins/dlib.cmake
+++ openbr/plugins/dlib.cmake
@@ -3,6 +3,8 @@ set(BR_WITH_DLIB OFF CACHE BOOL "Build with DLib")
 if(${BR_WITH_DLIB})
   find_package(DLib REQUIRED)
 
+  add_definitions(-DDLIB_NO_GUI_SUPPORT)
+
   set(BR_THIRDPARTY_SRC ${BR_THIRDPARTY_SRC} plugins/dlib.cpp)
   set(BR_THIRDPARTY_SRC ${BR_THIRDPARTY_SRC} ${DLib_DIR}/dlib/all/source.cpp)
 
