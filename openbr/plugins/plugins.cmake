c8b9c4dd8b5e3b03546a61b2bfc301384799a81f
Jordan Cheney <jordan.cheney@gmail.com>
2015-02-26 11:17:31 -0500

    Death to cmake errors

--- openbr/plugins/plugins.cmake
+++ openbr/plugins/plugins.cmake
@@ -1,6 +1,7 @@
 # Optional Appendable CMake Variables:
 # BR_THIRDPARTY_PLUGINS     - Additional plugins
 # BR_THIRDPARTY_PLUGINS_DIR - Additional folder(s) of plugins
+# BR_EXCLUDED_PLUGINS       - Plugins that should not be built
 # BR_THIRDPARTY_SRC         - Additional source code needed by a plugin
 # BR_THIRDPARTY_LIBS        - Additional libaries needed by a plugin
 
@@ -21,14 +22,15 @@ mark_as_advanced(BR_EXCLUDED_PLUGINS)
 foreach(DIR plugins/cmake ${BR_THIRDPARTY_PLUGINS_DIR})
   file(GLOB CMAKE_FILES ${DIR}/*.cmake)
   foreach(CMAKE_FILE ${CMAKE_FILES})
-    include(${CMAKE_FILE})
+    if (NOT ${CMAKE_FILE} MATCHES "Find.*cmake")
+      include(${CMAKE_FILE})
+    endif()
   endforeach()
 endforeach()
 
 # Collect all source files except for excluded plugins
 foreach(DIR ${BR_PLUGINS_DIR} ${BR_THIRDPARTY_PLUGINS_DIR})
   get_filename_component(DIR_NAME ${DIR} NAME)
-  set(AUTOMOC_TARGETS_FOLDER ${CMAKE_BINARY_DIR}/openbr/${DIR_NAME})
   file(GLOB PLUGINS ${DIR}/*.cpp ${DIR}/*.h)
   foreach(PLUGIN ${PLUGINS})
     get_filename_component(PLUGIN_NAME ${PLUGIN} NAME)
