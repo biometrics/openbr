457777b08c4c876b1f51dfd5467c84df372ecc4e
Scott Klum <scott.klum@gmail.com>
2015-03-11 13:23:24 -0400

    Better stasm inclusion/compilation

--- openbr/plugins/cmake/stasm4.cmake
+++ openbr/plugins/cmake/stasm4.cmake
@@ -2,14 +2,7 @@ set(BR_WITH_STASM4 ON CACHE BOOL "Build with Stasm")
 
 if(${BR_WITH_STASM4})
   find_package(Stasm4 REQUIRED)
-  set(BR_THIRDPARTY_LIBS ${BR_THIRDPARTY_LIBS} ${Stasm4_LIBS})
-
-  if(WIN32)
-    install(DIRECTORY ${Stasm_DIR}/build/ DESTINATION bin)
-  else()
-    install(DIRECTORY ${Stasm_DIR}/build/ DESTINATION lib)
-  endif()
-
+  set(BR_THIRDPARTY_SRC ${BR_THIRDPARTY_SRC} ${Stasm_SRC})
   install(DIRECTORY ${Stasm_DIR}/data/ DESTINATION share/openbr/models/stasm)
 else()
   set(BR_EXCLUDED_PLUGINS ${BR_EXCLUDED_PLUGINS} plugins/metadata/stasm4.cpp)
