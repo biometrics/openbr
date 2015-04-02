167eb072b40cc6b7da92f5a7a735c8326cbc3b79
Scott Klum <scott.klum@gmail.com>
2015-04-02 10:54:49 -0400

    Added support for multiple histograms

--- openbr/plugins/distance/emd.cpp
+++ openbr/plugins/distance/emd.cpp
@@ -8,9 +8,8 @@ namespace br
 
 /*!
  * \ingroup distances
- * \brief Fuses similarity scores across multiple matrices of compared templates
+ * \brief Computes Earth Mover Distance
  * \author Scott Klum \cite sklum
- * \note Operation: Mean, sum, min, max are supported.
  */
 class EMDDistance : public UntrainableDistance
 {
@@ -18,17 +17,26 @@ class EMDDistance : public UntrainableDistance
 
     float compare(const Template &a, const Template &b) const
     {
-        Mat sig_a(a.m().cols, 2, CV_32FC1);
-        Mat sig_b(b.m().cols, 2, CV_32FC1);
+        const int dims_a = a.m().rows > 1 ? 3 : 2;
+        const int dims_b = b.m().rows > 1 ? 3 : 2;
 
-        for (int i=0; i<a.m().cols; i++) {
-            sig_a.at<float>(i,0) = a.m().at<float>(0,i);
-            sig_a.at<float>(i,1) = i;
+        Mat sig_a(a.m().cols, dims_a, CV_32FC1);
+        Mat sig_b(b.m().cols, dims_b, CV_32FC1);
+
+        for (int i=0; i<a.m().rows; i++) {
+            for (int j=0; j<a.m().cols; j++) {
+                sig_a.at<float>(i*a.m().cols+j,0) = a.m().at<float>(i,j);
+                sig_a.at<float>(i*a.m().cols+j,1) = j;
+                if (dims_a == 3) sig_a.at<float>(i*a.m().cols+j,2) = i;
+            }
         }
 
-        for (int i=0; i<b.m().cols; i++) {
-            sig_b.at<float>(i,0) = b.m().at<float>(0,i);
-            sig_b.at<float>(i,1) = i;
+        for (int i=0; i<b.m().rows; i++) {
+            for (int j=0; j<b.m().cols; j++) {
+                sig_b.at<float>(i*b.m().cols+j,0) = b.m().at<float>(i,j);
+                sig_b.at<float>(i*b.m().cols+j,1) = j;
+                if (dims_b == 3) sig_a.at<float>(i*b.m().cols+j,2) = i;
+            }
         }
 
         return EMD(sig_a,sig_b,CV_DIST_L2);
