f54e33c13a4ad363c44930bda45d502150416825
Scott Klum <scott.klum@gmail.com>
2015-04-12 15:37:46 -0400

    Added border property

--- openbr/plugins/metadata/grid.cpp
+++ openbr/plugins/metadata/grid.cpp
@@ -29,16 +29,18 @@ class GridTransform : public UntrainableTransform
     Q_OBJECT
     Q_PROPERTY(int rows READ get_rows WRITE set_rows RESET reset_rows STORED false)
     Q_PROPERTY(int columns READ get_columns WRITE set_columns RESET reset_columns STORED false)
+    Q_PROPERTY(float border READ get_border WRITE set_border RESET reset_border STORED false)
     BR_PROPERTY(int, rows, 1)
     BR_PROPERTY(int, columns, 1)
+    BR_PROPERTY(float, border, 0)
 
     void project(const Template &src, Template &dst) const
     {
         QList<QPointF> landmarks;
-        const float row_step = 1.f * src.m().rows / rows;
-        const float column_step = 1.f * src.m().cols / columns;
-        for (float y=row_step/2; y<src.m().rows; y+=row_step)
-            for (float x=column_step/2; x<src.m().cols; x+=column_step)
+        const float row_step = (src.m().rows-src.m().rows*border*2) / rows;
+        const float column_step = (src.m().cols-src.m().cols*border*2) / columns;
+        for (float y=row_step/2+src.m().rows*border; y<src.m().rows-src.m().rows*border; y+=row_step)
+            for (float x=column_step/2+src.m().cols*border; x<src.m().cols-src.m().cols*border; x+=column_step)
                 landmarks.append(QPointF(x,y));
         dst = src;
         dst.file.setPoints(landmarks);
