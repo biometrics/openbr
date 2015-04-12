0f880500cd0ff0b6dcec0b68213f87e9d84c1aab
Scott Klum <scott.klum@gmail.com>
2015-04-12 15:36:43 -0400

    Fixed logic error in samplefrommask

--- openbr/plugins/imgproc/samplefrommask.cpp
+++ openbr/plugins/imgproc/samplefrommask.cpp
@@ -17,16 +17,14 @@ class SampleFromMaskTransform : public UntrainableTransform
     void project(const Template &src, Template &dst) const
     {
         Mat mask = src.file.get<Mat>("Mask");
-        const int count = countNonZero(mask);
+        Mat indices;
+        findNonZero(mask,indices);
 
-        if (count > 0) {
-            dst.m() = Mat(1,count,src.m().type());
+        if (indices.total() > 0) {
+            dst.m() = Mat(1,indices.total(),src.m().type());
 
             Mat masked;
             src.m().copyTo(masked, mask);
-
-            Mat indices;
-            findNonZero(masked,indices);
 
             for (size_t j=0; j<indices.total(); j++)
                 dst.m().at<uchar>(0,j) = masked.at<uchar>(indices.at<Point>(j).y,indices.at<Point>(j).x);
