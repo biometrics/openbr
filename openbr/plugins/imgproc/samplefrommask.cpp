7b0e83a18b1aa56634314188284b57c2600a4ee5
Scott Klum <scott.klum@gmail.com>
2015-04-23 10:40:40 -0400

    Refactored SampleFromMask a bit

--- openbr/plugins/imgproc/samplefrommask.cpp
+++ openbr/plugins/imgproc/samplefrommask.cpp
@@ -14,22 +14,37 @@ class SampleFromMaskTransform : public UntrainableTransform
 {
     Q_OBJECT
 
+    Q_PROPERTY(int minIndices READ get_minIndices WRITE set_minIndices RESET reset_minIndices STORED false)
+    BR_PROPERTY(int, minIndices, 0)
+
     void project(const Template &src, Template &dst) const
     {
+        dst = src;
+
         Mat mask = src.file.get<Mat>("Mask");
         Mat indices;
         findNonZero(mask,indices);
 
-        if (indices.total() > 0) {
-            dst.m() = Mat(1,indices.total(),src.m().type());
-
+        if (indices.total() > (size_t)minIndices) {
             Mat masked;
             src.m().copyTo(masked, mask);
+            if (src.m().channels() > 1) {
+                dst.m() = Mat(3,indices.total(),CV_32FC1);
+                for (size_t j=0; j<indices.total(); j++) {
+                    Vec3b v =  masked.at<Vec3b>(indices.at<Point>(j).y,indices.at<Point>(j).x);
+                    dst.m().at<float>(0,j) = v[0];
+                    dst.m().at<float>(1,j) = v[1];
+                    dst.m().at<float>(2,j) = v[2];
+                }
+            } else {
+                dst.m() = Mat(1,indices.total(),src.m().type());
 
-            for (size_t j=0; j<indices.total(); j++)
-                dst.m().at<uchar>(0,j) = masked.at<uchar>(indices.at<Point>(j).y,indices.at<Point>(j).x);
+                for (size_t j=0; j<indices.total(); j++)
+                    dst.m().at<uchar>(0,j) = masked.at<uchar>(indices.at<Point>(j).y,indices.at<Point>(j).x);
+            }
         } else {
             dst.file.fte = true;
+            dst.file.remove("Mask");
             qWarning("No mask content for %s.",qPrintable(src.file.baseName()));
         }
     }
