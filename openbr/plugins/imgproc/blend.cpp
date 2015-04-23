d7538510516f74995885d9803401074af5a48156
Scott Klum <scott.klum@gmail.com>
2015-04-23 10:24:32 -0400

    Added support for more than two matrices in blend

--- openbr/plugins/imgproc/blend.cpp
+++ openbr/plugins/imgproc/blend.cpp
@@ -29,13 +29,22 @@ namespace br
 class BlendTransform : public UntrainableMetaTransform
 {
     Q_OBJECT
-    Q_PROPERTY(float alpha READ get_alpha WRITE set_alpha RESET reset_alpha STORED false)
-    BR_PROPERTY(float, alpha, 0.5)
+    Q_PROPERTY(QList<float> alpha READ get_alpha WRITE set_alpha RESET reset_alpha STORED false)
+    BR_PROPERTY(QList<float>, alpha, QList<float>() << 0.5)
 
     void project(const Template &src, Template &dst) const
     {
-        if (src.size() != 2) qFatal("Expected two source matrices.");
-        addWeighted(src[0], alpha, src[1], 1-alpha, 0, dst);
+        dst.file = src.file;
+
+        if (src.size() != 2) {
+            dst.m() = Mat::zeros(src.m().rows, src.m().cols, src.m().type());
+            for (int i=0; i<src.size(); i++) {
+                const float weight = (alpha.size() == src.size()) ? alpha[i] : 1./src.size();
+                dst.m() += src[i]*weight;
+            }
+        } else {
+            addWeighted(src[0], alpha[0], src[1], 1-alpha[0], 0, dst);
+        }
     }
 };
 
