4a4c6882e1e3aeca8ef1c377a514d2b0c3918475
Josh Klontz <josh@rankone.io>
2015-06-08 13:29:32 -0400

    automatically build dlib by default

--- openbr/plugins/cmake/dlib.cmake
+++ openbr/plugins/cmake/dlib.cmake
@@ -1,10 +1,14 @@
-set(BR_WITH_DLIB OFF CACHE BOOL "Build with DLib")
+set(BR_WITH_DLIB ON CACHE BOOL "Build with DLib")
 
 if(${BR_WITH_DLIB})
-  find_package(DLib REQUIRED)
-  add_definitions(-DDLIB_NO_GUI_SUPPORT)
-  set(BR_THIRDPARTY_SRC ${BR_THIRDPARTY_SRC} ${DLib_DIR}/dlib/all/source.cpp)
-  install(DIRECTORY ${DLib_DIR}/models/ DESTINATION share/openbr/models/dlib)
+  ExternalProject_Add(dlib
+                      URL http://downloads.sourceforge.net/project/dclib/dlib/v18.16/dlib-18.16.tar.bz2
+                      URL_MD5 e9e5449bc25370afce2d254327afac99
+                      SOURCE_DIR "${CMAKE_SOURCE_DIR}/3rdparty/dlib-18.16"
+                      CONFIGURE_COMMAND ""
+                      BUILD_COMMAND ""
+                      INSTALL_COMMAND "")
+  include_directories("${CMAKE_SOURCE_DIR}/3rdparty/dlib-18.16")
 else()
   set(BR_EXCLUDED_PLUGINS ${BR_EXCLUDED_PLUGINS} plugins/classification/dlib.cpp)
 endif()
