706efbacd4c5213cfe1ddfaabc3adc942ffe7b49
Scott Klum <scott.klum@gmail.com>
2015-06-14 13:43:43 -0400

    Added CheckPoints

---
+++ openbr/plugins/metadata/checkpoints.cpp
@@ -0,0 +1,33 @@
+#include <openbr/plugins/openbr_internal.h>
+
+namespace br
+{
+
+/*!
+ * \ingroup transforms
+ * \brief Checks the points in a template for missing (-1,-1) values
+ * \author Scott Klum \cite sklum
+ */
+class CheckPointsTransform : public UntrainableMetadataTransform
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QList<int> indices READ get_indices WRITE set_indices RESET reset_indices STORED false)
+    BR_PROPERTY(QList<int>, indices, QList<int>())
+
+    void projectMetadata(const File &src, File &dst) const
+    {
+        dst = src;
+        for (int i=0; i<indices.size(); i++)
+            if (src.points()[indices[i]] == QPointF(-1,-1)) {
+                dst.fte = true;
+                break;
+            }
+    }
+};
+
+BR_REGISTER(Transform, CheckPointsTransform)
+
+} // namespace br
+
+#include "metadata/checkpoints.moc"
