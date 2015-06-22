be7b6a07d8e039acf1ad92a4550441b4a7e3f3de
bhklein <benhunter1990@gmail.com>
2015-06-22 16:56:19 -0400

    janus gallery updates

--- openbr/janus_io.cpp
+++ openbr/janus_io.cpp
@@ -12,32 +12,6 @@ static void _janus_add_sample(vector<double> &samples, double sample)
     samples.push_back(sample);
 }
 
-static void _janus_create_template(const char *data_path, TemplateData templateData, janus_gallery gallery, bool verbose)
-{
-    janus_template template_;
-    janus_template_id templateID;
-    JANUS_ASSERT(TemplateIterator::create(data_path, templateData, &template_, &templateID, verbose))
-    templateData.release();
-
-    static QMutex enrollLock;
-    QMutexLocker enrollLocker(&enrollLock);
-
-    JANUS_ASSERT(janus_enroll(template_, templateID, gallery))
-}
-
-janus_error janus_create_gallery(const char *data_path, janus_metadata metadata, janus_gallery gallery, int verbose)
-{
-    TemplateIterator ti(metadata, true);
-    TemplateData templateData = ti.next();
-    QFutureSynchronizer<void> futures;
-    while (!templateData.templateIDs.empty()) {
-        futures.addFuture(QtConcurrent::run(_janus_create_template, data_path, templateData, gallery, verbose));
-        templateData = ti.next();
-    }
-    futures.waitForFinished();
-    return JANUS_SUCCESS;
-}
-
 typedef QPair<janus_template_id, FlatTemplate> TemplatePair;
 
 TemplatePair _janus_create_flat_template(const char *data_path, TemplateData templateData, bool verbose)
@@ -47,6 +21,34 @@ TemplatePair _janus_create_flat_template(const char *data_path, TemplateData tem
     JANUS_ASSERT(TemplateIterator::create(data_path, templateData, &template_, &templateID, verbose))
     templateData.release();
     return TemplatePair(templateID, FlatTemplate(template_));
+}
+
+janus_error janus_create_gallery(const char *data_path, janus_metadata metadata, janus_gallery_path gallery_path, int verbose)
+{
+    TemplateIterator ti(metadata, true);
+    TemplateData templateData = ti.next();
+    QFutureSynchronizer<TemplatePair> futures;
+    while (!templateData.templateIDs.empty()) {
+        futures.addFuture(QtConcurrent::run(_janus_create_flat_template, data_path, templateData, verbose));
+        templateData = ti.next();
+    }
+    futures.waitForFinished();
+    QList< QFuture<TemplatePair> > flat_templates = futures.futures();
+
+    vector<janus_flat_template> templates;
+    vector<size_t> template_bytes;
+    vector<janus_template_id> template_ids;
+    size_t num_templates = 0;
+
+    foreach (const QFuture<TemplatePair> &future, flat_templates) {
+        template_ids.push_back(future.result().first);
+        template_bytes.push_back(future.result().second.data->bytes);
+        templates.push_back(future.result().second.data->flat_template);
+        num_templates++;
+    }
+
+    JANUS_ASSERT(janus_write_gallery(&templates[0], &template_bytes[0], &template_ids[0], num_templates, gallery_path))
+    return JANUS_SUCCESS;
 }
 
 janus_error janus_create_templates(const char *data_path, janus_metadata metadata, const char *gallery_file, int verbose)
