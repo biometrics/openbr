c33f46cc2204bd90745065581a71bb94086af98d
Jordan Cheney <jordan.cheney@gmail.com>
2015-08-04 12:29:01 -0400

    Fixes to Caffe and a new transform for classification

--- openbr/plugins/classification/caffe.cpp
+++ openbr/plugins/classification/caffe.cpp
@@ -63,7 +63,7 @@ private:
  * \br_link Caffe Integration Tutorial ../tutorials.md#caffe
  * \br_link Caffe website http://caffe.berkeleyvision.org
  */
-class CaffeFVTransform : public UntrainableTransform
+class CaffeFVTransform : public UntrainableMetaTransform
 {
     Q_OBJECT
 
@@ -90,26 +90,115 @@ class CaffeFVTransform : public UntrainableTransform
     {
         CaffeNet *net = caffeResource.acquire();
 
-        MemoryDataLayer<float> *data_layer = static_cast<MemoryDataLayer<float> *>(net->layers()[0].get());
+        if (net->layers()[0]->layer_param().type() != "MemoryData")
+            qFatal("OpenBR requires the first layer in the network to be a MemoryDataLayer");
 
-        if (src.size() != data_layer->batch_size())
-            qFatal("src should have %d (batch size) mats. It has %d mats.", data_layer->batch_size(), src.size());
+        MemoryDataLayer<float> *dataLayer = dynamic_cast<MemoryDataLayer<float> *>(net->layers()[0].get());
+
+        if (src.size() != dataLayer->batch_size())
+            qFatal("src should have %d (batch size) mats. It has %d mats.", dataLayer->batch_size(), src.size());
 
         dst.file = src.file;
 
-        data_layer->AddMatVector(src.toVector().toStdVector(), std::vector<int>(src.size(), 0));
+        dataLayer->AddMatVector(src.toVector().toStdVector(), std::vector<int>(src.size(), 0));
 
         Blob<float> *output = net->ForwardPrefilled()[1]; // index 0 is the labels from the data layer (in this case the 0 array we passed in above).
                                                           // index 1 is the ouput of the final layer, which is what we want
-        int dim_features = output->count() / data_layer->batch_size();
-        for (int n = 0; n < data_layer->batch_size(); n++)
-            dst += Mat(1, dim_features, CV_32FC1, output->mutable_cpu_data() + output->offset(n));
+        int dimFeatures = output->count() / dataLayer->batch_size();
+        for (int n = 0; n < dataLayer->batch_size(); n++)
+            dst += Mat(1, dimFeatures, CV_32FC1, output->mutable_cpu_data() + output->offset(n));
 
         caffeResource.release(net);
     }
 };
 
 BR_REGISTER(Transform, CaffeFVTransform)
+
+/*!
+ * \brief A transform that wraps the Caffe deep learning library. This transform expects the input to a given Caffe model to be a MemoryDataLayer.
+ * The output of the Caffe network is treated as a classifier with one node per class. Dst is set equal to src and a list of labels and confidences
+ * is stored in the metadata using the tags "Labels" and "Confidences". The size of the lists is equal to the batch size of the network. If the batch
+ * size is 1 the lists are converted to an integer and floating point value respectively and are stored in metadata using "Label" and "Confidence" instead.
+ * \author Jordan Cheney \cite jcheney
+ * \br_property QString model path to prototxt model file
+ * \br_property QString weights path to caffemodel file
+ * \br_property int gpuDevice ID of GPU to use. gpuDevice < 0 runs on the CPU only.
+ * \br_link Caffe Integration Tutorial ../tutorials.md#caffe
+ * \br_link Caffe website http://caffe.berkeleyvision.org
+ */
+class CaffeClassifierTransform : public UntrainableMetaTransform
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QString model READ get_model WRITE set_model RESET reset_model STORED false)
+    Q_PROPERTY(QString weights READ get_weights WRITE set_weights RESET reset_weights STORED false)
+    Q_PROPERTY(int gpuDevice READ get_gpuDevice WRITE set_gpuDevice RESET reset_gpuDevice STORED false)
+    BR_PROPERTY(QString, model, "")
+    BR_PROPERTY(QString, weights, "")
+    BR_PROPERTY(int, gpuDevice, -1)
+
+    Resource<CaffeNet> caffeResource;
+
+    void init()
+    {
+        caffeResource.setResourceMaker(new CaffeResourceMaker(model, weights, gpuDevice));
+    }
+
+    bool timeVarying() const
+    {
+        return gpuDevice < 0 ? false : true;
+    }
+
+    void project(const Template &src, Template &dst) const
+    {
+        CaffeNet *net = caffeResource.acquire();
+
+        if (net->layers()[0]->layer_param().type() != "MemoryData")
+            qFatal("Integrating OpenBr with caffe requires the first layer in the network to be a MemoryDataLayer");
+
+        MemoryDataLayer<float> *dataLayer = static_cast<MemoryDataLayer<float> *>(net->layers()[0].get());
+
+        if (src.size() != dataLayer->batch_size())
+            qFatal("src should have %d (batch size) mats. It has %d mats.", dataLayer->batch_size(), src.size());
+
+        dst = src;
+
+        dataLayer->AddMatVector(src.toVector().toStdVector(), std::vector<int>(src.size(), 0));
+
+        Blob<float> *output = net->ForwardPrefilled()[1]; // index 0 is the labels from the data layer (in this case the 0 array we passed in above).
+                                                          // index 1 is the ouput of the final layer, which is what we want
+
+        QList<int> labels; QList<float> confidences;
+
+        int dimFeatures = output->count() / dataLayer->batch_size();
+        for (int n = 0; n < dataLayer->batch_size(); n++) {
+            const float *data = output->cpu_data() + output->offset(n);
+
+            int maxIdx = -1; float maxVal = -std::numeric_limits<float>::max();
+            for (int d = 0; d < dimFeatures; d++) {
+                if (data[d] > maxVal) {
+                    maxVal = data[d];
+                    maxIdx = d;
+                }
+            }
+
+            labels.append(maxIdx);
+            confidences.append(maxVal);
+        }
+
+        if (labels.size() == 1) {
+            dst.file.set("Label", labels[0]);
+            dst.file.set("Confidence", confidences[0]);
+        } else {
+            dst.file.setList<int>("Labels", labels);
+            dst.file.setList<float>("Confidences", confidences);
+        }
+
+        caffeResource.release(net);
+    }
+};
+
+BR_REGISTER(Transform, CaffeClassifierTransform)
 
 } // namespace br
 
