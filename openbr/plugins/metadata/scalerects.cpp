22dc34c9fb2117a9855d70f4279b2807f31cb1f0
Scott Klum <scott.klum@gmail.com>
2015-09-10 12:05:17 -0400

    Added scalerects

---
+++ openbr/plugins/metadata/scalerects.cpp
@@ -0,0 +1,63 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright 2012 The MITRE Corporation                                      *
+ *                                                                           *
+ * Licensed under the Apache License, Version 2.0 (the "License");           *
+ * you may not use this file except in compliance with the License.          *
+ * You may obtain a copy of the License at                                   *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ * Unless required by applicable law or agreed to in writing, software       *
+ * distributed under the License is distributed on an "AS IS" BASIS,         *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ * See the License for the specific language governing permissions and       *
+ * limitations under the License.                                            *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <openbr/plugins/openbr_internal.h>
+#include <openbr/core/opencvutils.h>
+
+using namespace cv;
+
+namespace br
+{
+
+/*!
+ * \ingroup transforms
+ * \brief Creates a bounding square around three points (typically the two eyes and the chin).
+ * \author Scott Klum \cite sklum
+ * \br_property int index Index of rect to scale.
+ * \br_property int left Number of pixels to scale the left of the rect.
+ * \br_property int top Number of pixels to scale the top of the rect.
+ * \br_property int right Number of pixels to scale the right of the rect.
+ * \br_property int bottom Number of pixels to scale the bottom of the rect.
+ */
+class ScaleRectsTransform : public UntrainableTransform
+{
+    Q_OBJECT
+
+    Q_PROPERTY(int index READ get_index WRITE set_index RESET reset_index STORED false)
+    Q_PROPERTY(int left READ get_left WRITE set_left RESET reset_left STORED false)
+    Q_PROPERTY(int top READ get_top WRITE set_top RESET reset_top STORED false)
+    Q_PROPERTY(int right READ get_right WRITE set_right RESET reset_right STORED false)
+    Q_PROPERTY(int bottom READ get_bottom WRITE set_bottom RESET reset_bottom STORED false)
+    BR_PROPERTY(int, index, 0)
+    BR_PROPERTY(int, left, -15)
+    BR_PROPERTY(int, top, -25)
+    BR_PROPERTY(int, right, 15)
+    BR_PROPERTY(int, bottom, 5)
+
+    void project(const Template &src, Template &dst) const {
+        dst = src;
+        QList<QRectF> rects = src.file.rects();
+        QRectF rect = rects[index].adjusted(left, top, right, bottom);
+        rects.replace(index, rect);
+        dst.file.setRects(OpenCVUtils::toRects(rects));
+    }
+};
+
+BR_REGISTER(Transform, ScaleRectsTransform)
+
+} // namespace br
+
+#include "metadata/scalerects.moc"
