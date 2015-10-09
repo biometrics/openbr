658401dcf16f52c936540f9def97d16d98d5ef23
Scott Klum <scott.klum@gmail.com>
2015-10-09 12:58:40 -0400

    Minor bug fix in averagepoints

--- openbr/plugins/metadata/averagepoints.cpp
+++ openbr/plugins/metadata/averagepoints.cpp
@@ -14,23 +14,14 @@ class AveragePointsTransform : public UntrainableMetadataTransform
     Q_PROPERTY(QList<int> indices READ get_indices WRITE set_indices RESET reset_indices STORED false)
     Q_PROPERTY(QString metaName READ get_metaName WRITE set_metaName RESET reset_metaName STORED true)
     Q_PROPERTY(bool append READ get_append WRITE set_append RESET reset_append STORED true)
-    Q_PROPERTY(int nLandmarks READ get_nLandmarks WRITE set_nLandmarks RESET reset_nLandmarks STORED true)
     BR_PROPERTY(QList<int>, indices, QList<int>())
     BR_PROPERTY(QString, metaName, "")
     BR_PROPERTY(bool, append, false)
-    BR_PROPERTY(int, nLandmarks, 51)
 
     void projectMetadata(const File &src, File &dst) const
     {
         dst = src;
-        if (src.points().size() != nLandmarks) {
-            if (Globals->verbose)
-                qDebug() << "Warning: Face has " << src.points().size() << "points; should be " << nLandmarks;
-            dst.fte = true;
-            return;
-        }
-        int x1 = 0,
-            y1 = 0;
+        float x1 = 0, y1 = 0;
 
         for (int i = 0; i < indices.size(); i++) {
             x1 += src.points()[indices[i]].x();
