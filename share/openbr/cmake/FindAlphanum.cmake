24379d1c6590d0c84bd6b19cbece3d3ef16824d1
Josh Klontz <josh@rankone.io>
2015-11-11 15:03:24 +0000

    fix for cross-compilation

--- share/openbr/cmake/FindAlphanum.cmake
+++ share/openbr/cmake/FindAlphanum.cmake
@@ -1,3 +1,3 @@
-find_path(ALPHANUM_DIR alphanum.hpp ${PROJECT_SOURCE_DIR}/3rdparty/*)
+find_path(ALPHANUM_DIR NAMES alphanum.hpp PATHS ${PROJECT_SOURCE_DIR}/3rdparty/* NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
 mark_as_advanced(ALPHANUM_DIR)
 include_directories(${ALPHANUM_DIR})
