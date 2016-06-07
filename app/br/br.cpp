6b0f85a21d270715ba997e7d3303105eaacd593c
Charles Otto <ottochar@gmail.com>
2016-06-07 11:07:21 -0700

    Revert removal of br_slave_process

--- app/br/br.cpp
+++ app/br/br.cpp
@@ -223,6 +223,9 @@ public:
                 check(parc == 1, "Incorrect parameter count for 'daemon'.");
                 daemon = true;
                 daemon_pipe = parv[0];
+            } else if (!strcmp(fun, "slave")) {
+                check(parc == 1, "Incorrect parameter count for 'slave'");
+                br_slave_process(parv[0]);
             } else if (!strcmp(fun, "exit")) {
                 check(parc == 0, "No parameters expected for 'exit'.");
                 daemon = false;
