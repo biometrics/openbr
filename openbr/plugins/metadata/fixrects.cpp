4255592f02e300ec11c147edd7ee1c8e15b9ea78
KeyurPatel <kyr_patel@yahoo.com>
2017-01-30 21:21:23 +0000

    Code Review Changes

--- openbr/plugins/metadata/fixrects.cpp
+++ openbr/plugins/metadata/fixrects.cpp
@@ -25,6 +25,42 @@ class FixRectsTransform : public UntrainableTransform
 
 BR_REGISTER(Transform, FixRectsTransform)
 
+
+/*!
+ * \ingroup transforms
+ * \brief Checks the rects in a template for invalid values
+ * \author Keyur Patel \cite kpatel
+ */
+
+class CheckRectsTransform : public UntrainableTransform
+{
+    Q_OBJECT
+    Q_PROPERTY(bool removeBadRect READ get_removeBadRect WRITE set_removeBadRect RESET reset_removeBadRect STORED false)
+    BR_PROPERTY(bool, removeBadRect, true)
+
+    void project(const Template &src, Template &dst) const
+    {
+        dst = src;
+        dst.file.clearRects();
+        QList<QRectF> rects = src.file.rects();
+        foreach (QRectF r, rects){
+            if ((r.left() < 0) || (r.right() > src.m().cols-1) || (r.top() < 0) || (r.bottom() > src.m().rows-1)){
+                if (removeBadRect){
+                    rects.removeOne(r);
+                }
+                else {
+                    dst.file.fte = true;
+                    break;
+                }
+            }
+        }
+        dst.file.setRects(rects);
+    }
+};
+
+BR_REGISTER(Transform, CheckRectsTransform)
+
+
 } // namespace br
 
 #include "metadata/fixrects.moc"
