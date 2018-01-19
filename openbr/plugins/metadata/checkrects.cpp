b827ec88bf1a4efd66a9aa5ff6f7c54a611a3a7c
Keyur Patel <keyur@rankone.io>
2018-01-19 10:47:50 -0700

    preserve rect aspect ratio

--- openbr/plugins/metadata/checkrects.cpp
+++ openbr/plugins/metadata/checkrects.cpp
@@ -42,10 +42,10 @@ class CheckRectsTransform : public UntrainableTransform
         if (fixRects){
             for (int i=0; i<rects.size(); i++) {
                 QRectF r = rects[i];
-                if (r.left() < 0) r.setLeft(0);
-                if (r.right() > src.m().cols-1) r.setRight(src.m().cols-1);
-                if (r.top() < 0) r.setTop(0);
-                if (r.bottom() > src.m().rows-1) r.setBottom(src.m().rows-1);
+                if (r.left() < 0)                   r.moveLeft(0);
+                if (r.right() > src.m().cols-1)     r.moveRight(src.m().cols-1);
+                if (r.top() < 0)                    r.moveTop(0);
+                if (r.bottom() > src.m().rows-1)    r.moveBottom(src.m().rows-1);
                 dst.file.appendRect(r);
             }
         } else {
