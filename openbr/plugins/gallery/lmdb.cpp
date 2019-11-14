d36f82be4403d950502d638849889e10fa0d4322
Scott <scott@rankone.io>
2019-11-14 10:17:02 -0700

    Revert "sleep if write buffer gets too large"
    This reverts commit 567c8b9628b132afa38ad9a3a4bdda23523129cb.

--- openbr/plugins/gallery/lmdb.cpp
+++ openbr/plugins/gallery/lmdb.cpp
@@ -180,9 +180,6 @@ class lmdbGallery : public Gallery
             lock.unlock();
 
             aThread.waitForFinished();
-
-            if (data.size() > 10000)
-                QThread::msleep(1);
         }
     }
     
