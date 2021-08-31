218d689c665be1a33eff08077acf729c4f6e454e
Josh Klontz <josh@rankone.io>
2021-08-31 12:45:50 -0600

    removed plugins not supported by opencv4

--- openbr/plugins/cmake/opencv4.cmake
+++
@@ -1,4 +0,0 @@
-set(BR_EXCLUDED_PLUGINS ${BR_EXCLUDED_PLUGINS} plugins/classification/forest.cpp
-                                               plugins/imgproc/custom_sift.cpp
-                                               plugins/imgproc/keypointdescriptor.cpp
-                                               plugins/metadata/keypointdetector.cpp)
